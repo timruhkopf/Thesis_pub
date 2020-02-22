81bd9999a3c12cb8cad238e7d177072eced6e8f4
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-02-22 15:53:16 +0100

    commented TB related cmd for exec try

--- shell/remote_tb.sh
+++ shell/remote_tb.sh
@@ -1,20 +1,31 @@
+#!/bin/bash
 # Script to run Tensorboard from remote machine & forward it to local machine
 
 # ssh connection via port-forwarding
-ssh -L 16006:127.0.0.1:6006 user@remote
+ssh -L 16006:127.0.0.1:6006 truhkop@gwdu103.gwdg.de
 
 # Open Tensorboard from input
 module purge
-module load python/3.6.8
-source MA-3.6.3/bin/activate # activate venv
+module load python/3.6.3
+source $HOME/MA-3.6.3/bin/activate # activate venv
 
 # ToDo get input for partial dir
-tensorboard --logdir=results/logs
+echo 'current wd:'
+cd $HOME/results/logs
+# printing dir with no files
+# ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\// /g' -e 's/^/ /'
+find . | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
 
-# ToDo Open Chrome with port
-# FIXME: depending on which port is used!
-http://127.0.0.1:16006/
+echo 'Which Tensorboard dir would you like to launch?'
+read dir
+tensorboard --logdir=results/logs/$dir
+
+# Open Chrome with port
+xdg-open http://127.0.0.1:16006/
+read -p 'press any key to kill this port & terminate' -n1 -s
+
 
 # ToDo cleaning up the port
-# ps aux | grep ssh | grep truhkop
-# kill <id>
+# FIXME: depending on which port is used!
+ps aux | grep ssh | grep truhkop
+#kill <id>
