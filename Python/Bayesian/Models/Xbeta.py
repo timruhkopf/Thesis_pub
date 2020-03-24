e0692c9b938348e482ee824423e44aaa356a73d4
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-03-24 14:33:49 +0100

    Xbeta refactoring draft. sample_chain fails in particular with tracefn

--- Python/Bayesian/Models/Xbeta.py
+++ Python/Bayesian/Models/Xbeta.py
@@ -7,60 +7,79 @@ tfb = tfp.bijectors
 from Python.Bayesian.Samplers.AdaptiveHMC import AdaptiveHMC
 
 
-class Xbeta(AdaptiveHMC):
+class Xbeta:
+    def __init__(self, p):
+        """
+        linear regression with fixed, homoscedastic sigma.
+        :param p: number of parameters
+        """
+        self.p = p
+        self.prior_beta = tfd.Normal(loc=tf.repeat(0., p), scale=tf.repeat(1., p))
 
-    def __init__(self, xgrid, n, beta=[-1., 2.], prior_beta=tfd.Normal(loc=[0., 0.], scale=[1., 1.])):
-        # data generation
-        # MAKE SURE, all of the below is TF Ready
-
-        self.n = n
-        self.X = tf.stack([tf.ones((self.n,)),
-                           tfd.Uniform(xgrid[0], xgrid[1]).sample((self.n,))],
-                          axis=1)
-        self.beta = tf.constant(beta)
-        self.prior_beta = prior_beta
-        self.mu = tf.linalg.matvec(self.X, self.beta)
-        self.y = self.mu + tfd.Normal(loc=0, scale=1).sample((self.n,))
-        # TODO TRAIN TEST SPLIT?
-
-        # attributes for sampler
-        self.initial = tf.constant([1., 1.])  # CAREFULL MUST BE FLOAT!
-        self.bijectors = tfb.Identity()  # [tfb.Identity(), tfb.Identity()]
-
-        self.unnormalized_log_prob = self._closure_log_prob(
-            self.X, self.y, self.prior_beta)
-
-        AdaptiveHMC.__init__(self, initial=self.initial,
-                             bijectors=self.bijectors,
-                             log_prob=self.unnormalized_log_prob)
+    def _initialize_from_prior(self):
+        self.beta = self.prior_beta.sample((1,))
 
     def likelihood_model(self, X, beta):
-        # setting up likelihood model
         mu_hat = tf.linalg.matvec(X, beta)
         y = tfd.Normal(loc=mu_hat, scale=1.)  # distributional assumption
         return y, mu_hat
 
-    def _closure_log_prob(self, X, y, prior_beta):
+    def _closure_log_prob(self, X, y):
         """A closure, to set X, y & the priors in this model"""
 
         @tf.function
         def xbeta_log_prob(beta):
             likelihood, _ = self.likelihood_model(X, beta)
-            return (prior_beta.log_prob(beta) +
+            return (self.prior_beta.log_prob(beta) +
                     tf.reduce_sum(likelihood.log_prob(y)))
 
         return xbeta_log_prob
 
 
+class Data:
+    def __init__(self, xgrid, n, beta=None):
+        self.n = n
+        self.X = tf.stack(
+            values=[tf.ones((self.n,)),
+                    tfd.Uniform(xgrid[0], xgrid[1]).sample((self.n,))],
+            axis=1)
+
+        self.beta = tf.constant(beta)
+        self.y = self.true_likelihood().sample((self.n,))
+
+    def true_likelihood(self):
+        self.mu = tf.linalg.matvec(self.X, self.beta)
+        y = tfd.MultivariateNormalDiag(
+            loc=self.mu,
+            scale_diag=tf.repeat(1., self.mu.shape[0]))
+        return y
+
+
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
+    # (0) (generating data) ------------------------------
+    # beta = tfd.Normal(loc=[0., 0.], scale=[1., 1.]).sample((1,))
+    data = Data(xgrid=(0, 10), n=1000, beta=[-1., 2.])
 
-    xbeta = Xbeta(xgrid=(0, 10), n=1000, beta=[-1., 2.])
+    # TODO TRAIN TEST SPLIT?
+
+    # (1) (generating the model) ------------------------
+    xbeta = Xbeta(p=2)
+    xbeta.unnormalized_log_prob = xbeta._closure_log_prob(data.X, data.y)
     print(xbeta.unnormalized_log_prob(beta=tf.constant([-1., 2.])))
-    samples, traced = xbeta.sample_chain(
+
+    xbeta._initialize_from_prior()
+
+    # (2) (sampling the model) --------------------------
+    adHMC = AdaptiveHMC(initial=xbeta.beta,  # tf.constant([1., 1.]) # CAREFULL MUST BE FLOAT!
+                        bijectors=tfb.Identity(),  # [tfb.Identity(), tfb.Identity()]
+                        log_prob=xbeta.unnormalized_log_prob)
+
+    samples, traced = adHMC.sample_chain(num_burnin_steps=int(1e3), num_results=int(10e2),
         logdir='/home/tim/PycharmProjects/Thesis/TFResults')
 
+    # (3) (evaluate the model) --------------------------
     # histogram of parameters
     is_accepted = xbeta.traced.inner_results.is_accepted
     samples = samples.numpy()
