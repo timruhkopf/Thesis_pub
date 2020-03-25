14c63b5205299e720bce0a6f4346147e3ca602db
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-03-25 12:46:56 +0100

    BNN & Hidden debugged: final_layer is linear combo, init_W_from_prior, debugged forward path

--- Python/Bayesian/Models/BNN.py
+++ Python/Bayesian/Models/BNN.py
@@ -6,18 +6,28 @@ tfb = tfp.bijectors
 
 
 class Hidden:
-    def __init__(self, no_units=10, activation='relu'):
+    def __init__(self, input_shape, no_units=10, activation='relu'):
         """create independent normal priors with MVN(0,I)"""
 
-        self.prior_W = tfd.MultivariateNormalDiag(
-            loc=tf.repeat(0., no_units),
-            scale_diag=tf.repeat(1., no_units))
+        self.input_shape = input_shape
+        self.no_units = no_units
+
+        # W matrix is drawn with stacked vector
+        self.prior_stackedW = tfd.MultivariateNormalDiag(
+            loc=tf.repeat(0., no_units * input_shape),
+            scale_diag=tf.repeat(1., no_units * input_shape))
         self.prior_b = tfd.MultivariateNormalDiag(
             loc=tf.repeat(0., no_units),
             scale_diag=tf.repeat(1., no_units))
 
+        identity = lambda x: x
         self.activation = {'relu': tf.nn.relu,
-                           'tanh': tf.math.tanh}[activation]
+                           'tanh': tf.math.tanh,
+                           'identity': identity}[activation]
+
+    def init_W_from_prior(self):
+        """initialize W matrix from stacked_prior"""
+        return tf.reshape(self.prior_stackedW.sample(), shape=(self.no_units, self.input_shape))
 
     @tf.function
     def dense(self, x, W, b):
@@ -36,18 +46,29 @@ class BNN:
     # gaussian priors for for weights and biases.
     # https://www.cs.tufts.edu/comp/150BDL/2018f/assignments/hw2.html
 
-    def __init__(self, units=[10, 9, 8, 1], activation='relu'):
-        self.units = units
-        self.layers = [Hidden(u, activation) for u in units]
+    def __init__(self, hunits=[2, 10, 9, 8, 1], activation='relu'):
+        """
+        model specification:
+        :param hunits: list: number of hidden units per layer. first element is
+        input shape. last element is output_shape.
+        :param activation: activation function for
+        """
+        self.hunits = hunits
+        self.layers = [Hidden(i, u, activation) for i, u in zip(self.hunits[:-2], self.hunits[1:-1])]
+        self.final_layer = Hidden(input_shape=self.hunits[-2],
+                                  no_units=self.hunits[-1],
+                                  activation='identity')
 
     def _initialize_from_prior(self):
-        # FIXME: sample_shape!. Does list match Samplers.initial input format?
-        # FIXME: list comprehension or tf.map_fn(lambda i: i ** 2 if i > 0 else i, x)
-        self.Ws = [h.prior_W.sample() for h in self.layers]
-        self.b = [h.prior_b.sample() for h in self.layers]
+        # TODO: list comprehension or tf.map_fn(lambda i: i ** 2 if i > 0 else i, x)
+        # tf.map_fn(lambda x: x[0] + x[1], tf.stack([tf.constant([1., 2.]), tf.constant([3., 4.])], axis=1))
 
-    @tf.function
+        self.Ws = [h.init_W_from_prior() for h in [*self.layers, self.final_layer]]
+        self.bs = [h.prior_b.sample() for h in self.layers]
+
+    # @tf.function
     def forward(self, X, Ws, bs):
+        # TODO default arguments for Ws, bs - if already self.Ws and bs exist?
         """
         Evaluate the NN nested in BNN, once random variables are realized.
         :param X: Batch of x vectors
@@ -58,21 +79,29 @@ class BNN:
         # FIXME: map dense accross all rows of X! (as dense operates on vectors)
         for h, W, b in zip(self.layers, Ws, bs):
             X = h.dense(X, W, b)
+
+        X = self.final_layer.dense(X, Ws[-1], tf.constant(0.))
         return X
 
-    @tf.function
+    # @tf.function
     def likelihood_model(self, X, Ws, bs):
         mu = self.forward(X, Ws, bs)
         y = tfd.MultivariateNormalDiag(loc=mu, scale_diag=tf.repeat(1., mu.shape[0]))
         return y
 
     def _closure_log_prob(self, X, y):
-        """A closure, to set X, y in this model and get HMC's expected model format"""
+        """A closure, to preset X, y in this model and match HMC's expected model format"""
 
-        @tf.function
+        # @tf.function
         def BNN_log_prob(Ws, bs):
+            """unnormalized log posterior value: log_priors + log_likelihood"""
+            # Fixme: BNN_log_prob(Ws, bs) sample_chain(current_state= is either
+            #  tensor or list of tensors, but not multiple arguments!!!
+            # Consider (*args) and argparser(*args)
+
             likelihood, _ = self.likelihood_model(X, Ws, bs)
-            # unnormalized log posterior value: log_priors + log_likelihood
+
+            # FIXME: carefull with the final layers bs also stack W to vector!
             return (tf.reduce_sum(h.log_prob(W, b) for h, W, b in zip(self.layers, Ws, bs)) +
                     tf.reduce_sum(likelihood.log_prob(y)))
 
@@ -80,16 +109,25 @@ class BNN:
 
 
 if __name__ == '__main__':
-    # (0) (generating data) ----------------------------------
-    from Python.Data.Cases2D.K.GMRF_K import GMRF_K
+    h = Hidden(input_shape=2, no_units=3, activation='relu')
+    h.dense(x=tf.constant([1., 2.]),
+            W=tf.constant([[1., 1.], [1., 2.], [3., 4.]]),  # three hidden units
+            b=tf.constant([0.5, 1., 1.]))
 
-    data = GMRF_K(xgrid=(0, 10, 0.5), ygrid=(0, 10, 0.5))
+    # (0) (generating data) ----------------------------------
+    # data = Data_BNN_2D(n=1000, grid=(0, 10, 0.5))
 
     # (1) (setting up posterior model) -----------------------
-    bnn = BNN(units=[10, 9, 8, 1], activation='relu')
-    bnn.unnormalized_log_prob = bnn._closure_log_prob(data.X, data.y)
+    bnn = BNN(hunits=[2, 10, 9, 8, 1], activation='relu')
+    # data = Data_BNN_2D(n=1000, grid=(0, 10, 0.5))
+    # bnn.unnormalized_log_prob = bnn._closure_log_prob(data.X, data.y)
 
     # (1.1) (sampling NN from priors) ------------------------
+    bnn._initialize_from_prior()
+    y = bnn.forward(X=tf.constant([3.,4]), Ws=bnn.Ws, bs=bnn.bs)
+
+    # batches work naturally!
+    y = bnn.forward(X=tf.constant([[1., 2.],[3.,4]]), Ws=bnn.Ws, bs=bnn.bs)
 
 
     # (2) (sampling posterior) -------------------------------
