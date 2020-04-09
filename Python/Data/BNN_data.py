ddc1800409bb5df953c2833d80dc9d39c206aad7
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-04-09 10:03:04 +0200

    laying out grid for BNN_2D Data

--- Python/Data/BNN_data.py
+++ Python/Data/BNN_data.py
@@ -47,22 +47,39 @@ class Data_BNN_1D:
 class Data_BNN_2D:
     def __init__(self, n, grid=(0, 10, 0.5)):
         """complex two dimensional effect, no main effects"""
-        from Python.Effects.Cases2D.K.GMRF_K import GMRF_K
-        self.gmrf = GMRF_K(xgrid=grid, ygrid=grid)
-
         self.n = n
-        self.X = tf.stack(
-            values=[tfd.Uniform(grid[0], grid[1]).sample((self.n,)),
-                    tfd.Uniform(grid[0], grid[1]).sample((self.n,))],
+        self.grid = grid
+
+        self.X = self.prior_X()
+
+        self.prior()  # Consider self.prior().sample()
+        likelihood, mu = self.true_likelihood(self.X)
+        self.y = likelihood.sample()
+        self.mu = mu
+
+    def prior_X(self):
+        return tf.stack(
+            values=[tfd.Uniform(self.grid[0], self.grid[1]).sample((self.n,)),
+                    tfd.Uniform(self.grid[0], self.grid[1]).sample((self.n,))],
             axis=1)
-        self.y = self.true_likelihood(self.X).sample((self.n,))
+
+    def prior(self):
+        from Python.Effects.Cases2D.K.GMRF_K import GMRF_K
+        self.gmrf = GMRF_K(xgrid=self.grid, ygrid=self.grid)
+
+        # FIXME: should return a proper tfd. to allow easy log_prob:
+        #  alternative, if improper:
+        #  return gmrf
+        #  with methods:
+        #  self.gmrf.sample() und self.gmrf.log_prob()
+        #  BOTH MUST RETURN TF TENSOR
 
     def true_likelihood(self, X):
-        self.mu = tf.constant(self.gmrf.surface(X), dtype=tf.float32)
-        y = tfd.MultivariateNormalDiag(
-            loc=self.mu,
-            scale_diag=tf.repeat(1., self.mu.shape[0]))
-        return y
+        mu = tf.constant(self.gmrf.surface(X), dtype=tf.float32)
+        likelihood = tfd.MultivariateNormalDiag(
+            loc=mu,
+            scale_diag=tf.repeat(1., mu.shape[0]))
+        return likelihood, mu
 
 
 if __name__ == '__main__':
