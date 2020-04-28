b5c09cbba93a89e68aeeacadb46eabb768474561
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-04-28 12:54:30 +0200

    added functionality to display confidence bands

--- Python/Bayesian/plot1d.py
+++ Python/Bayesian/plot1d.py
@@ -13,13 +13,20 @@ def plot_tfd_1D(dist, support=tf.range(0., 20., 0.05)):
     var = dist.prob(support).numpy()
     sns.lineplot(support, var)
 
-def plot1d_functions(X, y, **kwargs):
+
+def plot1d_functions(X, y, confidence=None, **kwargs):
     """
     function to plot 1d Data and the function estimates of the data
     :param X: Tensor (n,1)
     :param y: Tensor (n,1)
+    :param confidence: optional dict, containing x, y1, y2 keys to plot
+    a shaded region between y1 and y2, representing some quantiles of the
+    posterior predictive conditional on x
+    # carefull x != X (x should be more fine grained!)
     :param kwargs: {'funcname': Tensor(n,1), ...}
+
     :example:
+    # (1) plot functions
     n = 200
     X = tfd.Uniform(-10., 10.).sample(n)
     Z = tf.convert_to_tensor(
@@ -30,6 +37,17 @@ def plot1d_functions(X, y, **kwargs):
     f_ols = gam_rw.dense(Z, gam_rw.OLS(Z, y))
 
     plot1d_functions(X,y, **{'true':f_true, 'init':f_init, 'ols': f_ols})
+
+    # (2) Confidence bands syntax:
+     x = tf.linspace(-10., 10., 100)
+    z = tf.convert_to_tensor(
+        get_design(x.numpy(), degree=2, no_basis=no_basis),
+        tf.float32)
+    mi = gam_rw.dense(z, true_param['W'])
+    ma = gam_rw.dense(z, init_param['W'])
+    plot1d_functions(X, y, confidence={'x': x, 'y1': mi, 'y2': ma},
+                     **{'ols': f_ols, 'true': f_true, 'init': f_init})
+
     """
 
     # convert to seaborn format
@@ -50,6 +68,9 @@ def plot1d_functions(X, y, **kwargs):
 
     sns.lineplot('X', y='vals', hue='functions', data=df, ax=ax)
 
+    if confidence is not None:
+        ax.fill_between(**confidence, alpha =0.4, facecolor='lightblue')
+
     fig.suptitle('Functions of the data')
     plt.plot()
 
@@ -58,7 +79,7 @@ if __name__ == '__main__':
     plot_tfd_1D(dist=tfd.InverseGamma(1., 1., name='tau'),
                 support=tf.range(0., 20., 0.05))
 
-    plot_tfd_1D(dist=tfd.InverseGamma(1.,1.))
+    plot_tfd_1D(dist=tfd.InverseGamma(1., 1.))
 
     # 2D example
     # from Python.Effects.bspline import diff_mat1D
