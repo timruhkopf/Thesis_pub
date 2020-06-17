2ed5d8a82fda42d51aa1c74bf3753812dab13c3b
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-06-17 11:26:29 +0200

    init for TF samplers MALA & pSGLD

---
+++ Tensorflow/Bayesian/Samplers/MALA.py
@@ -0,0 +1,10 @@
+# https://www.tensorflow.org/probability/api_docs/python/tfp/mcmc/MetropolisAdjustedLangevinAlgorithm
+from Tensorflow.Bayesian.Samplers.Samplers import Samplers
+
+class MALA(Samplers):
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ Tensorflow/Bayesian/Samplers/pSGLD.py
@@ -0,0 +1,10 @@
+# https://www.tensorflow.org/probability/api_docs/python/tfp/optimizer/StochasticGradientLangevinDynamics
+
+from Tensorflow.Bayesian.Samplers import Samplers
+
+class pSGLD(Samplers):
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
