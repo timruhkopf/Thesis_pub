fd38812f3f9f8657c5ab7bc303b3f990ce0f12ba
Tim Ruhkopf <timruhkopf@googlemail.com>
2020-06-17 11:26:13 +0200

    abstracted BNN_GAM -- i.e. the switch of w^~ from the rest of the model-- merely instantiation and replacing the first layer with the appropriate shrinkage version of default Hidden
    Also init for shrinkage scripts

--- Tensorflow/Bayesian/Models/Shrinkage/BNN_GAM_lasso.py
+++ Tensorflow/Bayesian/Models/Shrinkage/BNN_GAM_lasso.py
@@ -1,5 +1,6 @@
 from Tensorflow.Bayesian.Models.Base.BNN import BNN
 from Tensorflow.Bayesian.Models.Base.GAM_RW import GAM_RW
+from Tensorflow.Bayesian.Models.Base.BNN_GAM import BNN_GAM
 from Tensorflow.Bayesian.PriorModels.HiddenGroupLasso import HiddenGroupLasso
 from Tensorflow.Effects.bspline import get_design
 
@@ -20,7 +21,7 @@ tfd = tfp.distributions
 tfb = tfp.bijectors
 
 
-class BNN_GAM_lasso:
+class BNN_GAM_lasso(BNN_GAM):
     @setkwargs
     def __init__(self, no_basis=20, hunits=[2, 10, 9, 8, 1], activation='relu'):  # TODO gam_RW parameters
         """
@@ -41,131 +42,6 @@ class BNN_GAM_lasso:
         # Likelihood
         self.parameters_list.append('sigma')
         self.bijectors.append(tfb.Exp())
-
-    def likelihood_model(self, X, Z, bnn_param, gam_param):
-        # GAM & BNN prediction sum as mu of Normal.
-        # CAREFULL! GAM VALUE is "support" of prediction.
-        #  BNN calculates on the residual information (if additional interactions exist)
-
-        return tfd.JointDistributionNamed(OrderedDict(
-            sigma=tfd.InverseGamma(1., 1.),
-            y=lambda sigma: tfd.Sample(
-                tfd.Normal(loc=self.forward(X, Z, bnn_param, gam_param),
-                           scale=sigma))))
-
-    def forward(self, X, Z, bnn_param, gam_param):
-        # bnn.forward leverages each PriorModels dense method, which in this thesis
-        # consist only of W, b or W alone. (essentially deterministic Neural Net parameter)
-        nn_param = [{k: v for k, v in hparam.items() if k in ['W', 'b']}
-                    for hparam in bnn_param]
-
-        return tf.add(self.bnn.forward(X, nn_param),
-                      # TODO: tfp.bijector on [0,1] bnn_param['tausq']*
-                      tf.reshape(self.gam.dense(Z, gam_param['W']), (X.shape[0], 1)))
-
-    def _closure_log_prob(self, X, Z, y):
-        def _log_prob(*tensorlist):
-            # parameter parsing
-            gam_param, bnn_param, sigma = self.parse_tensorlist(tensorlist)
-
-            likelihood = self.likelihood_model(X, Z, bnn_param, gam_param)
-
-            return tf.reduce_sum(likelihood.log_prob(y=y, sigma=sigma)) + \
-                   self.bnn._layer_log_prob(bnn_param) + \
-                   self.gam.rw.prior_log_prob(gam_param)
-
-        return _log_prob
-
-    def parse_tensorlist(self, tensorlist):
-        """:returns tuple gam_param, bnn_param, sigma, with the first two being
-        dicts of parameter: tensor (cleaned of sigma), sigma is a tensor"""
-        gam_param, bnn_param, sigma = tensorlist[:len(self.gam.parameters) - 1], \
-                                      tensorlist[len(self.gam.parameters) - 1:-1], \
-                                      tensorlist[-1]
-
-        bnn_param = self.bnn.parse_tensorlist(bnn_param)
-        gam_param = {k: v for k, v in zip(self.gam.parameters, gam_param)}
-
-        return gam_param, bnn_param, sigma
-
-    def sample(self):
-        gam_param = self.gam.rw.sample()
-        bnn_param = self.bnn._layer_sample()
-        return gam_param, bnn_param
-
-    def flat_initialize(self, X_bnn, gam_ols=None):
-        """
-        :param ols: {'X':Z, 'y':y} to initialize gam with OLS estimate for W.
-        :return: list of tensors
-        """
-        # delegate initialization
-        gam_param = self.gam.rw.sample()
-        if gam_ols is not None:
-            gam_param['W'] = self.gam.OLS(**gam_ols)
-            # gam_param['sigma']
-
-        self.gam_init = gam_param
-
-        # remove sigma from gam & flatten gam_param
-        gam_param = list(v for k, v in gam_param.items() if k != 'sigma')
-
-        # CAREFULL THIS ALSO APPENDS SIGMA of BNN's Normal likelihood!
-        bnn_param = self.bnn.flat_initialize(X_bnn)
-        return gam_param + bnn_param
-
-    def plot2d_functions(self, gmrf2d, X_gam=None, y_gam=None, title='Effect', gam_funcs={}, bnn_funcs={}):
-        """plotting gam (1d) & bnn model (2d)'s TRUE partial residuals plots.
-        they are exacly known!"""
-
-        # for irregular grid data make use of Delauny triangulation & trisurf:
-        # https://fabrizioguerrieri.com/blog/2017/9/7/surface-graphs-with-irregular-dataset
-
-        df = pd.DataFrame({'X': tf.reshape(X_gam, (X_gam.shape[0],)).numpy()})
-        for name, var in gam_funcs.items():
-            df[name] = tf.reshape(var, (var.shape[0],)).numpy()
-        df = df.melt('X', value_name='y')
-        df = df.rename(columns={'variable': 'functions'})
-
-        (meshx, meshy), _ = gmrf2d.gmrf.grid
-        # meshx, meshy = np.meshgrid(x, y, indexing='ij')
-        gridxy = np.stack((meshx, meshy), axis=-1)
-
-        fig = plt.figure()
-        plt.title('{}'.format(title))
-
-        # plot coefficents without TP-Splines
-        ax1 = fig.add_subplot(221, projection='3d')
-        ax1.plot_wireframe(meshx, meshy, gmrf2d.gmrf.z.reshape(meshx.shape), color='C1')
-        ax1.set_title('Coefficents at grid position')
-
-        # plot the bnn mean surface
-        ax2 = fig.add_subplot((222), projection='3d')
-        x1 = gmrf2d.X[:, 0]
-        x2 = gmrf2d.X[:, 1]
-        mu = gmrf2d.gmrf.surface(gmrf2d.X)
-        y = gmrf2d.y
-        triang = triangulate_remove_artifacts(x1, x2, 0.1, 9.9, 0.1, 9.9, plot=False)
-
-        ax2.plot_trisurf(triang, mu, cmap='jet', alpha=0.4)  # groud truth function (approximation
-        ax2.scatter(x1, x2, y, marker='.', s=10, c="black", alpha=0.5)
-        ax2.view_init(elev=60, azim=-45)
-
-        for name, prediction in bnn_funcs.items():
-            ax2.plot_trisurf(triang, prediction, cmap='jet', alpha=0.4)
-
-        # ax2.set_title('TE-Spline with plugged-in gmrf-coef.')
-
-        # ax2.plot_surface(meshx, meshy, gmrf2d.gmrf.surface(gridxy), rstride=8, cstride=8, alpha=0.3)
-        # ax2.scatter(gmrf2d.X[:, 0].numpy(), gmrf2d.X[:, 1].numpy(), gmrf2d.y)
-
-        # plot the gam functions
-        ax3 = fig.add_subplot(224)
-        sns.scatterplot(
-            x=tf.reshape(X_gam, (X_gam.shape[0],)).numpy(),
-            y=y_gam.numpy() + surface.y - surface.mu, ax=ax3)
-        sns.lineplot('X', y='y', hue='functions', alpha=0.7, data=df, ax=ax3)
-
-        plt.show()
 
 
 if __name__ == '__main__':
@@ -249,7 +125,7 @@ if __name__ == '__main__':
 
     samples, traced = adHMC.sample_chain(
         num_burnin_steps=int(10e2),
-        num_results=int(10e3),
+        num_results=int(10e2),
         logdir='/home/tim/PycharmProjects/Thesis/TFResults')
 
     parameters = [tensorlist for tensorlist in zip(*samples)]
@@ -291,8 +167,6 @@ if __name__ == '__main__':
     #
     #     plt.draw()
     #     plt.pause(.001)
-
-
 
     # (USING TE SURFACE AS TARGET) --------------------------------------------
     from Tensorflow.Data.K_Surface import K_surface
---
+++ Tensorflow/Bayesian/PriorModels/HiddenGroupSpikeNSlab.py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+tfd = tfp.distributions
+tfb = tfp.bijectors
+
+from Tensorflow.Bayesian.PriorModels.Hidden import Hidden
+
+
+class HiddenGroupSpikeNSlab(Hidden):
+    def __init__(self, input_shape, no_units=10, activation='relu'):
+        pass
