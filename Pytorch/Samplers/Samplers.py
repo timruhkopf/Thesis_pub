620f46d10e9f215a7644d780fef85e3924cc036f
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-06-25 13:02:13 +0200

    try after username change

---
+++ Pytorch/Samplers/Samplers.py
@@ -0,0 +1,8 @@
+class Samplers:
+    def predict(self):
+        for paramset in self.chain:
+            # deserialize it potentially
+            # paralleliize the for loop:
+            # https://stackoverflow.com/questions/9786102/how-do-i-parallelize-a-simple-python-loop
+
+            pass
