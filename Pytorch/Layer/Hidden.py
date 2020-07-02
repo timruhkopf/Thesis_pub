2e61d1351a015e2ae81362a0bb15c78611780a66
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-07-02 15:52:15 +0200

    Hidden & BNN as flexible layer design

--- Pytorch/Layer/Hidden.py
+++ Pytorch/Layer/Hidden.py
@@ -1,43 +1,102 @@
 import torch
 import torch.distributions as td
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class Hidden:
+class Hidden(nn.Module): # (nn.Module):
     # TODO : see how to subclass torch layer
-    activ = {'identity': lambda x: x}  # fixme: Torch.Identity?
-
-    # 'tanh':
-    # 'relu':
-    # 'sigmoid':
+    activ = {'identity': lambda x: x,
+             'relu': lambda x: F.relu(x),
+             'sigmoid': lambda x: F.sigmoid(x),
+             'tanh': lambda x: F.tanh(x)}
 
     def __init__(self, no_in, no_units, bias=True, activation='relu'):
+        # super(Hidden, self).__init__()
+        super().__init__()
         self.no_in = no_in
         self.no_units = no_units
         self.bias = bias
-        self.activation = self.activ[activation]
 
-        self.parameters = ['W']
+        self.activation = self.activ[activation]
+        self.linear = nn.Linear(no_in, no_units, bias)
+
+        # self.parameters = ['W']
         # self.bijectors = ['Identity']  FIXME Bijectors: has Torch functionals?
 
-        self.W_prior = td.Normal(0., 1.)  # fixme: check implementation with hyperparam tau & joint density!
+        # fixme: check implementation with hyperparam tau & joint density!
+        self.W_prior = td.Normal(torch.zeros(no_units * no_in),
+                                 torch.tensor([1.]))
+
+        self.tau_b = 1.
         if self.bias is not None:
-            self.parameters.append('b')
+            # self.parameters.append('b')
             # self.bijectors.append('Identity')
-            self.b_prior = None
+            self.b_prior = td.Normal(torch.zeros(no_units), self.tau_b)
+
+        # Initialize the layer
+        self.sample()
 
     def sample(self):
-        self.W_prior.sample()  # fixme: check implementation
-        if self.bias is not None:
-            pass
-        else:
-            bias = 0.
+        """Sampling the weight and biases from standard Normal distributions
+        sampling a model causes the history to be removed potentially."""
+        self.linear.weight = nn.Parameter(
+            self.W_prior.sample().view(self.no_units, self.no_in))
+        if self.bias:
+            self.linear.bias = nn.Parameter(self.b_prior.sample())
 
-    def log_prob(self):
-        if self.bias is not None:
-            pass
+    def prior_log_prob(self):
+        log_prob = self.W_prior.log_prob(self.linear.weight.view(self.no_in * self.no_units)).sum()
+        if self.bias:
+            log_prob += self.b_prior.log_prob(self.linear.bias).sum()
 
-    def dense(self, X, W, b=None):
-        if self.bias is not None:
-            self.activation(X.mm(W))
-        else:
-            self.activation(X.mm(W) + b)
+        return log_prob
+
+    def __call__(self, X):
+        """:returns the mean i.e. XW^T """
+        return self.activation(self.linear(X))
+
+
+if __name__ == '__main__':
+    # replacing Parameters after Sampling values for them.
+    x = nn.Linear(10, 1)
+    x(torch.ones(10))
+    x.bias = nn.Parameter(torch.tensor([1.]), True)
+    x(torch.ones(10))
+    x.weight.sum() + 1.  # (+ bias)
+
+    # Regression example (only prior model) -------------------------------------------------
+    reg = Hidden(no_in=2, no_units=1, bias=True, activation='identity')
+
+    X_dist = td.Uniform(torch.tensor([-10., -10]), torch.tensor([10., 10]))
+    X = X_dist.sample(torch.Size([100]))
+    beta = torch.tensor([[1., 2.]])
+    reg.linear.weight = nn.Parameter(beta)
+    mu = reg(X)
+
+    # test log prob
+    reg.prior_log_prob()
+
+    # 2 consecutive Hidden Unit example --------------------------------------------------------------
+    X_dist = td.Uniform(torch.tensor([-10., -10]), torch.tensor([10., 10]))
+    X = X_dist.sample(torch.Size([100]))
+
+    first_layer = Hidden(no_in=2, no_units=10, bias=True, activation='relu')
+
+    W = torch.ones(10,2)
+    first_layer.linear.weight = nn.Parameter(W)
+    first_out = first_layer(X)
+
+    second_layer = Hidden(no_in=first_out.shape[1], no_units=1, bias=True, activation='identity')
+    mu = second_layer(first_out)
+
+    # prior model log_prob
+    first_layer.prior_log_prob()
+    second_layer.prior_log_prob()
+
+    # check the sampling function once more
+    first_layer.sample()
+    second_layer.sample()
+    first_layer.prior_log_prob()
+    second_layer.prior_log_prob()
+
--- Pytorch/Models/BNN.py
+++ Pytorch/Models/BNN.py
@@ -1,32 +1,138 @@
 import torch
+import torch.nn as nn
+import torch.distributions as td
+import torch.distributions.transforms as bij
+
 from itertools import chain
+from functools import partial
+import Pytorch.utils as utils
 from Pytorch.Layer.Hidden import Hidden
 
 
-class BNN:
+class BNN(nn.Module):
+    # coding example of BNN with MCMC
+    # https://github.com/OscarJHernandez/bayesian_neural_networks/blob/master/Code/BNN.py
     activ = Hidden.activ
 
-    def __init__(self, hunits=[1, 10, 5], activation='relu'):
+    def __init__(self, hunits=[1, 10, 5], activation='relu', final_activation='identity'):
+        """
+        Bayesian Neural Network, consisting of hidden layers.
+        :param hunits: list of integers, specifying the input dimensions of hidden units
+        :param activation: each layers activation function (except the last)
+        :param final_activation: activation function of the final layer.
+        :remark: see Hidden.activ for available activation functions
+        """
+        super().__init__();
         self.hunits = hunits
+
+        # LAYER PRIOR MODEL
+        # for the definition of adaptive layer sizes, consult
+        # https://discuss.pytorch.org/t/when-should-i-use-nn-modulelist-and-when-should-i-use-nn-sequential/5463/19
+        # on ModuleList & Sequential to register the parameters
         self.layers = [Hidden(no_in, no_units, activation)
                        for no_in, no_units in zip(self.hunits[:-2], self.hunits[1:-1])]
-        self.layers.append(Hidden(input_shape=hunits[-2], no_units=hunits[-1],
-                                  bias=False, activation='identity'))
+        self.layers.append(Hidden(no_in=hunits[-2], no_units=hunits[-1],
+                                  bias=False, activation=final_activation))
+        self.module_layers = nn.ModuleList(self.layers)
+        # self.Ls = nn.Sequential(*self.layers) # DEPREC
 
-    @property
-    def parameters(self):
-        return list(chain(*[list(h.parameters) for h in self.layers]))
+        # Variance Prior model
+        self.SIGMA = td.TransformedDistribution(td.Gamma(0.1, 0.1), bij.ExpTransform())
+        self.sigma = nn.Parameter(self.SIGMA.sample())
 
-    @property
-    def bijectors(self):
-        return list(chain(*[list(h.bijectors) for h in self.layers]))
-
-    def forward(self, X, param):
-        pass
+        self.log_prob = None  # placeholder for closure to be set
 
     def sample(self):
-        """sample the Prior Model"""
-        pass
+        """delegate the sampling of parameters to each layer"""
+        for h in self.layers:
+            h.sample()
 
-    def log_prob(self):
-        pass
+    def forward(self, X):
+        for h in self.module_layers:
+            X = h(X)  # call to "dense method XW' "
+        return X
+
+    def prior_log_prob(self):
+        return torch.stack([l.prior_log_prob() for l in self.layers], dim=0).sum(dim=0)
+
+    def likelihood(self, X, sigmasq):
+        return td.MultivariateNormal(self.forward(X), sigmasq * torch.eye(X.shape[0]))
+
+    def closure_log_prob(self, X=None, y=None):
+        """
+
+        :param X: (Optional) Batched Tensor.
+        :param y: (Optional) Batched Tensor.
+        :return: if both X & y are provided, a log_prob function is added to bnn,
+        which has pre set X & y, making it a full-dataset log_prob. If neither are
+        supplied, the added log_prob function requires X & y's for every call on
+        log_prob. However, this enables the SG behaviour of the log_prob function.
+        """
+
+        def log_prob(self, X, y, theta):
+            """By default works as an SG flavour, can be fixed using self.fix_log_prob(X,y)
+            to make it a full dataset sampler"""
+
+            # INPLACE ASSIGNMENT OF THETA ON NET WEIGHTS & param
+            utils.unflatten(vec=theta, model=self)
+
+            return self.likelihood(X, self.sigma).log_prob(y).sum() + \
+                   self.SIGMA.log_prob(self.sigma) + \
+                   self.prior_log_prob()
+
+        if isinstance(X, torch.Tensor) and isinstance(y, torch.Tensor):
+            print('"Full Dataset" log prob is being set up...')
+            if any([X is None, y is None]):
+                raise ValueError('"Full Dataset" modus requires specification of X & y')
+            self.log_prob = partial(log_prob, self, X, y)
+        else:
+            print('SG log prob is being set up...')
+            self.log_prob = partial(log_prob, self)
+
+
+if __name__ == '__main__':
+    # check bnn functions on simple regression data
+    # data pre-setup
+    from Pytorch.Layer.Hidden import Hidden
+    from Pytorch.utils import flatten, unflatten
+
+    reg = Hidden(no_in=2, no_units=1, bias=True, activation='identity')
+    X_dist = td.Uniform(torch.tensor([-10., -10]), torch.tensor([10., 10]))
+    X = X_dist.sample(torch.Size([100]))
+    beta = torch.tensor([[1., 2.]])
+    reg.linear.weight = nn.Parameter(beta)
+    mu = reg(X)
+
+    bnn = BNN(hunits=[2, 10, 1], activation='relu', final_activation='identity')
+
+    # generate y!
+    y = td.Normal(mu, torch.tensor(1.)).sample()
+
+    bnn.forward(X)
+    l = bnn.likelihood(X, torch.tensor(1.))
+    l.log_prob(y).sum()
+
+    # test full dataset log_prob
+    bnn.closure_log_prob(X, y)
+    bnn.sample()
+    theta = flatten(bnn)
+    # theta = torch.ones(41) *2
+    print(bnn.log_prob(theta))
+
+    # TODO: test sg log_prob:
+    #   check that each and every sg sampler accepts this format
+    #   particularly that theta can be at the last postion!
+    bnn.closure_log_prob()
+    bnn.log_prob(X=X, y=y, theta=theta)
+
+    # PARTIAL FUNCTION EXAMPLE
+    # from functools import partial
+    # # A normal function
+    # def f(a, b, c, x):
+    #     return 1000 * a + 100 * b + 10 * c + x
+    # # A partial function that calls f with
+    # # a as 3, b as 1 and c as 4.
+    # g = partial(f, 3, 1, 4)
+    #
+    # # Calling g()
+    # print(g(5))
