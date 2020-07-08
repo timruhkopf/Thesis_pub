110a768a48aea448472ad6578158ec223d6316f3
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-07-08 13:04:29 +0200

    updated imports & init arguments, making them compatible with other layers inputs - thereby iterable

--- Pytorch/Layer/Group_SpikeNSlab.py
+++ Pytorch/Layer/Group_SpikeNSlab.py
@@ -1,6 +1,9 @@
+import torch
+import torch.nn as nn
+
 from Pytorch.Layer.Hidden import Hidden
 
 
 class Group_SpikeNSlab(Hidden):
-    def __init__(self):
+    def __init__(self, no_in, no_out, bias=True, activation=nn.ReLU()):
         pass
--- Pytorch/Layer/Group_lasso.py
+++ Pytorch/Layer/Group_lasso.py
@@ -1,8 +1,11 @@
+import torch
+import torch.nn as nn
+
 from Pytorch.Layer.Hidden import Hidden
 
 
 class Group_lasso(Hidden):
     # inherritance allows to keep dense method & "activ" activation functions in one place!
     # IDEALLY MAKE IT AUTOGRAD READY IF NECESSARY BY SAMPLER - INHERRIT nn.Module?
-    def __init__(self):
+    def __init__(self, no_in, no_out, bias=True, activation=nn.ReLU()):
         self.bias = False  # to meet the Hidden default standard!
