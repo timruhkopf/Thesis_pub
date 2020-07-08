4c3a93ece6323f29cdfcee375e6ef492ef3febf7
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-07-08 09:59:08 +0200

    samplable GAM, but still: make tau chooseable!
    two modes K & cum form Tensorflow.Effects.Cases1D

--- Pytorch/Models/GAM.py
+++ Pytorch/Models/GAM.py
@@ -1,90 +1,92 @@
 import torch
 import torch.distributions as td
 import torch.nn as nn
-from functools import partial
+
 import Pytorch.utils as utils
+from Pytorch.Layer.Hidden import Hidden
 
 import numpy as np
 from Tensorflow.Effects.bspline import get_design, diff_mat1D
+from Tensorflow.Effects.Cases1D.Bspline_K import Bspline_K
+from Tensorflow.Effects.Cases1D.Bspline_cum import Bspline_cum
 
 
-class GAM(nn.Module):
-    def __init__(self, no_basis, order=1):
+class GAM(Hidden):
+    def __init__(self, order=1, no_basis=10, no_out=1, activation=nn.Identity(), ):
         """
-        RandomWalk Prior Model on Gamma
+        RandomWalk Prior Model on Gamma (W) vector.
+        Be carefull to transform the Data beforehand with some DeBoor Style algorithm.
+        This Module mereley implements the Hidden behaviour + Random Walk Prior
         :param no_basis: number of basis from the de boor basis expansion to expect (=no_columns of X)
+        which is in fact the no_in of Hidden.
         :param order: difference order to create the Precision (/Penalty) matrix K
         """
-        super(GAM, self).__init__()
+        super().__init__(no_basis, no_out, bias=False, activation=activation)
         self.order = order
         self.no_basis = no_basis
+        self.no_in = no_basis
         self.K = torch.tensor(diff_mat1D(no_basis, order)[1], dtype=torch.float32, requires_grad=False)
-        self.cov = torch.inverse(self.K[1:, 1:])
-        self.linear = nn.Linear(no_basis, 1, bias=False)
+        self.cov = torch.inverse(self.K[1:, 1:])  # FIXME: Multivariate Normal cholesky decomp fails!
 
-        # FIXME: log_transform??
-        self.TAU_GAMMA = td.TransformedDistribution(td.Gamma(0.1, 0.1), td.ExpTransform())
-        self.tau_gamma = nn.Parameter(self.TAU_GAMMA.sample())
+        self.tau_ = nn.Parameter(torch.Tensor(1))
+        self.dist_tau = td.Gamma(0.1, 0.1)
 
-        self.SIGMA = td.TransformedDistribution(td.Gamma(0.1, 0.1), td.ExpTransform())
-        self.sigma = nn.Parameter(self.SIGMA.sample())
+        self.sample()
 
-    def sample(self):
-        """sample the prior model"""
-        gamma = torch.cat(
-            [td.Uniform(torch.tensor([-1.]), torch.tensor([1.])).sample(),
-             td.MultivariateNormal(torch.zeros(self.no_basis - 1), self.cov).sample()],
-            dim=0).view(1, self.no_basis)
-        self.linear.weight = nn.Parameter(gamma)
-        self.tau_gamma = nn.Parameter(self.TAU_GAMMA.sample())
+    def sample(self, xgrid=(0, 10, 0.5), tau=1., mode='K'):
+        """
+        Sample the prior model, instantiating the data model
+        :param xgrid: defining space for the Bspline expansion. Notice, that
+        int((xgrid[1] - xgrid[0]) // 0.5) == self.no_basis is required!
+        :param tau: if not None, it is the inverse variance (smoothness) of
+        randomwalkprior. If None, the self.dist_tau is used for sampling tau
+        # FIXME: carefull with sigma/tau=lambda relation
+        :param mode: 'K' or 'cum'. 'K' specifies, that the model is sampled based
+        on the 1/tau * null-space-penalized precision matrix, which is plugged into MVN
+        :return: None. Inplace self.W, self.tau
+        """
+
+        if int((xgrid[1] - xgrid[0]) // 0.5) != self.no_basis:
+            raise ValueError('The Specified range(*xgrid) does not imply '
+                             'no_basis (len(range) must be equal to no_basis)')
+        if tau is None:
+            # FIXME: carefull with transformed distributions! (need to transform tau back)
+            self.tau = self.dist_tau.sample()
+        else:
+            self.tau = tau
+
+        # FIXME: need to specify tau / variance)
+        if mode == 'K':
+            bspline_k = Bspline_K(xgrid, no_coef=self.no_basis, order=1, sig_Q=0.1, sig_Q0=0.01, threshold=10 ** -3)
+            self.W = torch.tensor(bspline_k.z, dtype=torch.float32).view(self.no_out, self.no_basis)
+            self.W_.data = self.W
+
+        elif mode == 'cum':
+            bspline_cum = Bspline_cum(xgrid, coef_scale=0.3)
+            self.W = torch.tensor(bspline_cum.z, dtype=torch.float32).view(self.no_out, self.no_basis)
+            self.W_.data = self.W
+
+        else:
+            raise ValueError('Mode is incorreclty specified')
+
+        # gamma = torch.cat(
+        #     [td.Uniform(torch.tensor([-1.]), torch.tensor([1.])).sample(),
+        #      td.MultivariateNormal(torch.zeros(self.no_basis - 1), self.cov).sample()],
+        #     dim=0).view(self.no_out, self.no_basis)
+        # self.W = gamma
+        # self.W_.data = self.W
+        #
+        # self.tau = self.dist_tau.sample()
+        # self.tau_.data = self.tau
 
     def prior_log_prob(self):
-        """returns: log_probability sum of of gamma & tau_gamma """
-        gamma = self.linear.weight[0]
-
-        const = torch.log(2 * np.pi * self.tau_gamma)
-        kernel = (2 * self.tau_gamma) ** -1 * gamma @ self.K @ gamma.t()
-        return -const - kernel + \
-               self.TAU_GAMMA.log_prob(self.tau_gamma)
-
-    def forward(self, X):
-        """:returns the mean i.e. XW^T """
-        return self.linear(X)
-
-    # STANDALONE GAM MODEL FOR ESTIMATION:
-    def likelihood(self, X, sigmasq):
-        return td.Normal(self.forward(X), sigmasq)
-
-    def closure_log_prob(self, X=None, y=None):
         """
-
-        :param X: (Optional) Batched Tensor.
-        :param y: (Optional) Batched Tensor.
-        :return: if both X & y are provided, a log_prob function is added to bnn,
-        which has pre set X & y, making it a full-dataset log_prob. If neither are
-        supplied, the added log_prob function requires X & y's for every call on
-        log_prob. However, this enables the SG behaviour of the log_prob function.
+        returns: log_probability sum of gamma & tau and is calculating the
+         RandomWalkPrior
         """
-
-        def log_prob(self, X, y, theta):
-            """By default works as an SG flavour, can be fixed using self.fix_log_prob(X,y)
-            to make it a full dataset sampler"""
-
-            # INPLACE ASSIGNMENT OF THETA ON NET WEIGHTS & param
-            utils.unflatten(vec=theta, model=self)
-
-            return self.likelihood(X, self.sigma).log_prob(y).sum() + \
-                   self.SIGMA.log_prob(self.sigma) + \
-                   self.prior_log_prob()
-
-        if isinstance(X, torch.Tensor) and isinstance(y, torch.Tensor):
-            print('"Full Dataset" log prob is being set up...')
-            if any([X is None, y is None]):
-                raise ValueError('"Full Dataset" modus requires specification of X & y')
-            self.log_prob = partial(log_prob, self, X, y)
-        else:
-            print('SG log prob is being set up...')
-            self.log_prob = partial(log_prob, self)
+        const = torch.log(torch.tensor(torch.sqrt(2 * np.pi * self.tau)))  # fixme: check if tau is correct here!
+        kernel = (2 * self.tau) ** -1 * self.W @ self.K @ self.W.t()
+        return sum(-const - kernel + self.dist_tau.log_prob(self.tau))
 
 
 if __name__ == '__main__':
@@ -92,35 +94,36 @@ if __name__ == '__main__':
     no_basis = 20
     X_dist = td.Uniform(-10., 10)
     X = X_dist.sample(torch.Size([100]))
-    Z = torch.tensor(get_design(X.numpy(), degree=2, no_basis=no_basis), dtype=torch.float32, requires_grad=True)
+    X.detach()
+    Z = torch.tensor(get_design(X.numpy(), degree=2, no_basis=no_basis), dtype=torch.float32, requires_grad=False)
+    Z.detach()
 
-    gam = GAM(no_basis, order=1)
+    gam = GAM(no_basis=no_basis, order=1)
     gam.sample()
-    mu = gam(Z)
+    gam(Z)
 
-    y = gam.likelihood(Z, torch.tensor(10.)).sample()
-
-    # sampling Example
-    gam.sample()
+    y = gam.likelihood(Z).sample()
+    y.detach()
+    y.requires_grad_()
 
     # prior log prob example
     gam.prior_log_prob()
 
     # log_prob sg example
     theta = utils.flatten(gam)
-    gam.closure_log_prob()
-    gam.log_prob(Z, y, theta)
+    # gam.closure_log_prob()
+    # gam.log_prob(Z, y, theta)
 
     # log_prob full dataset example
     gam.closure_log_prob(Z, y)
     gam.log_prob(theta)
 
     # plot 1D
-    print()
 
+    # sample
     import hamiltorch
 
-    N = 200
+    N = 2000
     hamiltorch.set_random_seed(123)
 
     step_size = 0.15
@@ -130,8 +133,8 @@ if __name__ == '__main__':
     softabs_const = 10 ** 6
     L = 25
 
-    params_irmhmc_bij = hamiltorch.sample(log_prob_func=gam.log_prob, params_init=theta, num_samples=N,
-                                          step_size=step_size, num_steps_per_sample=L, sampler=hamiltorch.Sampler.RMHMC,
-                                          integrator=hamiltorch.Integrator.IMPLICIT, fixed_point_max_iterations=1000,
-                                          fixed_point_threshold=1e-05)
-
+    params_irmhmc_bij = hamiltorch.sample(
+        log_prob_func=gam.log_prob, params_init=theta, num_samples=N,
+        step_size=step_size, num_steps_per_sample=L, sampler=hamiltorch.Sampler.RMHMC,
+        integrator=hamiltorch.Integrator.IMPLICIT, fixed_point_max_iterations=1000,
+        fixed_point_threshold=1e-05)
