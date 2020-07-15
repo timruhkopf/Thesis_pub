ed28e0167438e57fab7c2bff0c090c608cf5800c
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-07-15 11:14:58 +0200

    eddited skeleton : structured prior in init on W reset_parameters, update_distributions. missing still:priro_log_prob (which should be similar to group_lasso

--- Pytorch/Layer/Group_HorseShoe.py
+++ Pytorch/Layer/Group_HorseShoe.py
@@ -20,12 +20,17 @@ class Group_HorseShoe(Hidden):
 
         self.dist = dict()
 
+        self.tau_ = nn.Parameter(torch.Tensor(1))
+        self.dist['tau'] = td.HalfCauchy(1.)
+        self.tau = self.dist['tau'].sample()
+
         # Group lasso structure of W
         self.W_ = nn.Parameter(torch.Tensor(self.no_in, self.no_out))
         self.W = None
         # self.dist['W_shrinked'] = td.Normal(torch.zeros(self.no_in), self.tau)
         # FIXME: check sigma dependence in W_shrinked: \beta_g | tau²_g, sigma² ~ MVN
         self.dist['W'] = td.Normal(torch.zeros(self.no_in * (self.no_out - 1)), 1.)
+        self.dist['W_shrinked'] = td.Normal(torch.zeros(self.no_in), self.tau)
 
         # add optional bias
         if bias:
@@ -33,12 +38,35 @@ class Group_HorseShoe(Hidden):
             self.b = None
             self.tau_b = 1.
             self.b = None
-            self.dist['b'] = td.Normal(0., 1.)
+            self.dist['b'] = td.Normal(0., self.tau_b)
 
-    def update_distributions(self):
-        pass
+        self.reset_parameters()
 
-    def reset_parameters(self):
+    def update_distributions(self, tau):
+        self.dist['W'].scale = tau
+
+    def reset_parameters(self, seperated=False):
+
+        if seperated:
+            raise NotImplementedError('still need to figure this out')
+        else:
+            self.tau = self.dist['tau'].sample()
+
+        self.tau = self.dist['tau'].sample()
+        self.dist['W_shrinked'].scale = self.tau
+
+        self.W = torch.cat([self.dist['W_shrinked'].sample().view(self.no_in, 1),
+                            self.dist['W'].sample().view(self.no_in, self.no_out - 1)],
+                           dim=1)
+
+        if self.bias:
+            self.b = self.dist['b'].sample()
+            self.b_.data = self.b
+
+        self.tau_ = self.tau
+        self.W_.data = self.W
+
+    def prior_log_prob(self):
         pass
 
     @property
@@ -49,4 +77,8 @@ class Group_HorseShoe(Hidden):
 
 
 if __name__ == '__main__':
-    pass
+    no_in = 2
+    no_out = 10
+
+    # single Hidden Unit Example
+    ghorse = Group_HorseShoe(no_in, no_out, bias=True, activation=nn.Identity())
