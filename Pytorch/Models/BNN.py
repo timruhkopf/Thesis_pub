c30461be09cbf5f3ca17a014f2699e0732163ab3
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-07-16 09:28:21 +0200

    delegate the sampling procedure even with first layer potentially having "seperated" attribute

--- Pytorch/Models/BNN.py
+++ Pytorch/Models/BNN.py
@@ -5,6 +5,7 @@ import torch.distributions as td
 from hamiltorch.util import flatten, unflatten
 from functools import partial
 from itertools import accumulate
+import inspect
 
 from Pytorch.Layer import *
 
@@ -35,6 +36,12 @@ class BNN(nn.Module):
             self.sigma = self.dist_sigma.sample()
         else:
             self.sigma = torch.tensor(1.)
+
+        self.true_model = None
+
+    @property
+    def vec(self):
+        return torch.cat([h.vec for h in self.layers])
 
     def forward(self, *args, **kwargs):
         return self.layers(*args, **kwargs)
@@ -71,16 +78,28 @@ class BNN(nn.Module):
         i.e. where sigma param is in likelihood"""
 
         # delegate the vector parts to the layers
-        lengths = list(accumulate([0] +[h.n_params for h in self.layers]))
+        lengths = list(accumulate([0] + [h.n_params for h in self.layers]))
         for i, j, h in zip(lengths, lengths[1:], self.layers):
             h.vec_to_attrs(vec[i:j])
 
         if self.heteroscedast:
             self.__setattr__('sigma', vec[-1])
 
+    def reset_parameters(self, seperated=False):
+        inspected = inspect.getfullargspec(self.layers[0].reset_parameters).args
+        if 'seperated' in inspected:
+            self.layers[0].reset_parameters(seperated)
+        else:
+            # default case: all Hidden units, no shrinkage -- has no seperated attr.
+            self.layers[0].reset_parameters()
+
+        for h in self.layers[1:]:
+            h.reset_parameters()
+
+
 
 if __name__ == '__main__':
-    bnn = BNN(hunits=[1,10,5,1])
+    bnn = BNN(hunits=[1, 10, 5, 1])
 
     # generate data
     X_dist = td.Uniform(torch.tensor(-10.), torch.tensor(10.))
@@ -94,9 +113,12 @@ if __name__ == '__main__':
     bnn.layers(X)
     bnn.forward(X)
 
+    bnn.reset_parameters()
+    bnn.true_model = bnn.vec
+
     # check vec_to_attrs
-    bnn.vec_to_attrs(torch.cat([i*torch.ones(h.n_params) for i, h in enumerate(bnn.layers)]))
-    [h.W for h in bnn.layers] # notice this does not cover the optional bias
+    bnn.vec_to_attrs(torch.cat([i * torch.ones(h.n_params) for i, h in enumerate(bnn.layers)]))
+    [h.W for h in bnn.layers]  # notice this does not cover the optional bias
     bnn.vec_to_attrs(torch.ones(80))
     bnn.forward(X)
 
@@ -120,7 +142,6 @@ if __name__ == '__main__':
         log_prob_func=bnn.log_prob, params_init=init_theta, num_samples=N,
         step_size=step_size, num_steps_per_sample=L)
 
-
     # check heteroscedastic case
     het_bnn = BNN(heteroscedast=True)
     het_bnn.forward(X)
@@ -137,8 +158,3 @@ if __name__ == '__main__':
         step_size=step_size, num_steps_per_sample=L)
 
     print()
-
-
-
-
-
