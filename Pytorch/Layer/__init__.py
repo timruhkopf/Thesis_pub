455482579195ee219fe756ca1df1527e39b9ed1f
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-07-22 15:14:50 +0200

    decomissioned spikeNslab

--- Pytorch/Layer/__init__.py
+++ Pytorch/Layer/__init__.py
@@ -1,4 +1,3 @@
 from Pytorch.Layer.Group_lasso import Group_lasso
-from Pytorch.Layer.Group_SpikeNSlab import Group_SpikeNSlab
 from Pytorch.Layer.Group_HorseShoe import Group_HorseShoe
 from Pytorch.Layer.Hidden import Hidden
