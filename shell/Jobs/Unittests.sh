27994ada4f4cd612b528dcb7f775c51d1646e813
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-08-12 10:40:42 +0200

    pip freeze

--- shell/Jobs/Unittests.sh
+++ shell/Jobs/Unittests.sh
@@ -3,15 +3,18 @@
 
 
 # NOTICE Execution via bash shell/Jobs/Unittests.sh
+# install packages:
+# python3 -m  pip install torch
+# python3 -m pip install git+https://github.com/AdamCobb/hamiltorch
 module load python/3.8.2
 
 
 echo "#### conquering the snake ####"
 cd /home/tim/PycharmProjects/Thesis/
-# python pip freeze
+python3 pip freeze
 
 # make sure not to add .py ending when calling a module file
-python -m Pytorch.Experiments.Hidden_Nuts # &>/home/tim/PycharmProjects/Thesis/Pytorch/Experiments/results/
-python -m Pytorch.Experiments.GAM_NUTS
+python3 -m Pytorch.Experiments.Hidden_Nuts # &>/home/tim/PycharmProjects/Thesis/Pytorch/Experiments/results/
+python3 -m Pytorch.Experiments.GAM_NUTS
 # python /home/uni08/truhkop/Masterthesis/Python/test_server_main.py &>/home/uni08/truhkop/results &
 wait
