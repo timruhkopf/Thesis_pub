cc9e56496254af523e7709048810e0c51f2dc425
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-08-25 14:20:55 +0200

    BNN as "OPTIM model only" works with SGNHT with small step size + plotting works

--- Pytorch/Models/BNN.py
+++ Pytorch/Models/BNN.py
@@ -6,10 +6,8 @@ from hamiltorch.util import flatten
 from itertools import accumulate
 import inspect
 
-from Pytorch.Layer.Layer_Probmodel.Hidden_Probmodel import Hidden_ProbModel
 from Pytorch.Layer.Hidden import Hidden
-from Pytorch.Util.ModelUtil import Vec_Model, Model_util, Optim_Model
-from thirdparty_repo.ludwigwinkler.src.MCMC_ProbModel import ProbModel
+from Pytorch.Util.ModelUtil import Model_util
 
 
 class BNN(nn.Module, Model_util):
@@ -37,17 +35,10 @@ class BNN(nn.Module, Model_util):
     # CLASSICS METHODS ---------------------------------------------------------
     def define_model(self):
         # Defining the layers depending on the mode.
-        if isinstance(self, Vec_Model):
-            L = Hidden
-        elif isinstance(self, Optim_Model):
-            L = Hidden_ProbModel
-        else:
-            L = Hidden
-
         self.layers = nn.Sequential(
-            *[L(no_in, no_units, True, self.activation)
+            *[Hidden(no_in, no_units, True, self.activation)
               for no_in, no_units in zip(self.hunits[:-2], self.hunits[1:-1])],
-            L(self.hunits[-2], self.hunits[-1], bias=False, activation=self.final_activation))
+            Hidden(self.hunits[-2], self.hunits[-1], bias=False, activation=self.final_activation))
 
         if self.heteroscedast:
             self.sigma_ = nn.Parameter(torch.Tensor(1))
@@ -86,14 +77,6 @@ class BNN(nn.Module, Model_util):
         return td.Normal(self.forward(X), scale=self.sigma)
 
     # SURROGATE (AGGREGATING) METHODS ------------------------------------------
-    @property
-    def vec(self):
-        return torch.cat([h.vec for h in self.layers])
-
-    @property
-    def parameters_list(self):
-        return [h.parameters_list for h in self.layers]
-
     def forward(self, *args, **kwargs):
         return self.layers(*args, **kwargs)
 
@@ -101,110 +84,55 @@ class BNN(nn.Module, Model_util):
         for h in self.layers:
             h.update_distributions()
 
-    def vec_to_attrs(self, vec):
-        """surrogate for Hidden Layers' vec_to_attrs, but actually needs to sett a
-        BNN.attribute in case of e.g. heteroscedasticity,
-        i.e. where sigma param is in likelihood"""
-
-        # delegate the vector parts to the layers
-        lengths = list(accumulate([0] + [h.n_params for h in self.layers]))
-        for i, j, h in zip(lengths, lengths[1:], self.layers):
-            h.vec_to_attrs(vec[i:j])
-
-        if self.heteroscedast:
-            self.__setattr__('sigma', vec[-1])
-
-
-
-class BNN_VEC(BNN, Vec_Model):
-    def __init__(self, *args, **kwargs):
-        nn.Module.__init__(self)
-        BNN.__init__(self, *args, **kwargs)
-
-
-class BNN_OPTIM(BNN, ProbModel, Optim_Model):
-    def __init__(self, *args, **kwargs):
-        nn.Module.__init__(self)
-        BNN.__init__(self, *args, **kwargs)
-
 
 if __name__ == '__main__':
-    # bnn = BNN(hunits=[1, 10, 5, 1])
-    #
-    # # generate data
-    # X_dist = td.Uniform(torch.tensor(-10.), torch.tensor(10.))
-    # X = X_dist.sample(torch.Size([100])).view(100, 1)
-    # y = bnn.likelihood(X).sample()
-    #
-    # # check forward path
-    # bnn.layers(X)
-    # bnn.forward(X)
-    #
+    no_in = 1
+    no_out = 1
+    n = 1000
+    bnn = BNN(hunits=[no_in, 10, 5, no_out], activation=nn.ReLU())
+
+    # generate data
+    X_dist = td.Uniform(torch.ones(no_in) * (-10.), torch.ones(no_in) * 10.)
+    X = X_dist.sample(torch.Size([n])).view(n, no_in)
+
+    from copy import deepcopy
+    bnn.reset_parameters() # true_model
+    bnn.true_model = deepcopy(bnn.state_dict())
+    y = bnn.likelihood(X).sample()
+
     # bnn.reset_parameters()
-    # bnn.true_model = bnn.vec
-    #
-    # # check vec_to_attrs
-    # bnn.vec_to_attrs(torch.cat([i * torch.ones(h.n_params) for i, h in enumerate(bnn.layers)]))
-    # bnn.parameters_list
-    # bnn.vec_to_attrs(torch.ones(80))
-    # bnn.forward(X)
-    #
-    # # check accumulation of parameters & parsing
-    # bnn.log_prob(X, y, flatten(bnn))
+    bnn.plot(X, y)
 
-    # (VEC MODEL & Data generation) --------------------------------------------
-    vec = BNN_VEC(hunits=[1, 10, 5, 1], activation=nn.ReLU(), final_activation=nn.Identity(), heteroscedast=False)
-    X_dist = td.Uniform(torch.tensor(-10.), torch.tensor(10.))
-    X = X_dist.sample(torch.Size([100])).view(100, 1)
-    y = vec.likelihood(X).sample()
+    # check forward path
+    bnn.layers(X)
+    bnn.forward(X)
 
+    bnn.prior_log_prob()
 
-    vec.closure_log_prob(X, y)
-    vec.log_prob(flatten(vec))
+    # check accumulation of parameters & parsing
+    bnn.log_prob(X, y)
 
-    import hamiltorch
-    import hamiltorch.util
+    from Pytorch.Samplers.LudwigWinkler import SGNHT, SGLD, MALA
 
-    init_theta = flatten(vec)
+    param = dict(step_size=0.001,
+                 num_steps=5000,  # <-------------- important
+                 pretrain=False,
+                 tune=False,
+                 burn_in=2000,
+                 # num_chains 		type=int, 	default=1
+                 num_chains=1,  # os.cpu_count() - 1
+                 hmc_traj_length=24)
 
-    # HMC NUTS
-    N = 200
-    step_size = .3
-    L = 5
-    burn = 500
-    N_nuts = burn + N
-    params_hmc_nuts = hamiltorch.sample(
-        log_prob_func=vec.log_prob, params_init=init_theta,
-        num_samples=N_nuts, step_size=step_size, num_steps_per_sample=L,
-        sampler=hamiltorch.Sampler.HMC_NUTS, burn=burn,
-        desired_accept_rate=0.8)
-
-    # (Optim model) ------------------------------------------------------------
-    from Pytorch.Samplers.LudwigWinkler import LudwigWinkler
-
-    optim = BNN_OPTIM()
-    ludi = LudwigWinkler(optim, X, y, batch_size=X.shape[0])
-
-    num_samples = 200
-    sampler = 'sgnht'
-    step_size = 0.1
-    num_steps = 100
-    pretrain = False
-    tune = False
-    burn_in = 2000
-    # num_chains 		type=int, 	default=1
-    num_chains = 1  # os.cpu_count() - 1
     batch_size = 50
-    hmc_traj_length = 20
     val_split = 0.9  # first part is train, second is val i.e. val_split=0.8 -> 80% train, 20% val
     val_prediction_steps = 50
     val_converge_criterion = 20
     val_per_epoch = 200
 
-    ludi.sample_SGNHT(step_size, num_steps, burn_in, pretrain=False, tune=tune, hmc_traj_length=hmc_traj_length,
-                      num_chains=num_chains)
 
-    type(ludi.sampler.chain)
-    ludi.sampler.chain.__dict__
+    sgnht = SGNHT(bnn, X, y, X.shape[0], **param)
+    sgnht.sample()
+    print(sgnht.chain)
 
-    print()
+    import random
+    sgnht.model.plot(X, y, random.sample(sgnht.chain, len(sgnht.chain) // 10))
