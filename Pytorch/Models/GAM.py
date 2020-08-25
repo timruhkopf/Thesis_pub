4dfed0fb55a8037dd9b4e315e0d8a26caa3d380e
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-08-25 14:34:51 +0200

    succeeded with GAM, sgnht in optim-based-only model

--- Pytorch/Models/GAM.py
+++ Pytorch/Models/GAM.py
@@ -1,7 +1,6 @@
 import torch
 import torch.distributions as td
 import torch.nn as nn
-from hamiltorch.util import flatten, unflatten
 
 from Pytorch.Layer.Hidden import Hidden
 from Pytorch.Util.DistributionUtil import LogTransform
@@ -47,17 +46,15 @@ class GAM(Hidden):
         Hidden.__init__(self, no_basis, no_out, bias=False, activation=activation)
 
     def define_model(self):
-
-        self.tau_ = nn.Parameter(torch.Tensor(1))
-        self.tau = torch.tensor([1.])
+        self.tau = nn.Parameter(torch.Tensor(1))
         self.dist['tau'] = td.Gamma(0.1, 0.1)
 
         if self.bijected:
             self.dist['tau'] = td.TransformedDistribution(self.dist['tau'], LogTransform())
             self.tau_bij = self.dist['tau'].transforms[0]._inverse(self.tau)
 
-        self.W_ = nn.Parameter(torch.Tensor(self.no_in, self.no_out))
-        self.W = None
+        self.W = nn.Parameter(torch.Tensor(self.no_in, self.no_out))
+
         # self.dist['W'] (RANDOMWALK PRIOR) formulated explicitly in prior model!
 
     def update_distributions(self):
@@ -94,10 +91,10 @@ class GAM(Hidden):
                              'no_basis (len(range) must be equal to no_basis)')
         if tau is None:
             # FIXME: carefull with transformed distributions! (need to transform tau back)
-            self.tau = self.dist_tau.sample()
+            self.tau.data = self.dist_tau.sample()  #
 
         else:
-            self.tau = tau
+            self.tau.data = tau
         self.update_distributions()
 
         # FIXME: need to specify tau / variance)
@@ -105,24 +102,21 @@ class GAM(Hidden):
         if mode == 'K':  # with nullspace-penalized K as precision for MVN
             bspline_k = Bspline_K(self.xgrid, no_coef=self.no_basis, order=1,
                                   sig_Q=0.1, sig_Q0=0.01, threshold=10 ** -3)
-            self.W = torch.tensor(bspline_k.z, dtype=torch.float32).view(self.no_basis, self.no_out)
+            self.W.data = torch.tensor(bspline_k.z, dtype=torch.float32).view(self.no_basis, self.no_out)
 
         elif mode == 'cum':  # sample based on actual cumulative randomwalk
             bspline_cum = Bspline_cum(self.xgrid, coef_scale=0.3)
-            self.W = torch.tensor(bspline_cum.z, dtype=torch.float32).view(self.no_out, self.no_basis)
+            self.W.data = torch.tensor(bspline_cum.z, dtype=torch.float32).view(self.no_out, self.no_basis)
 
         elif mode == 'U-MVN' and self.penK:  # Uniform for gamma0 & MVN (0, K[1:, 1:]**-1) as cov
             gamma = torch.cat(
                 [td.Uniform(torch.tensor([-1.]), torch.tensor([1.])).sample(),
                  td.MultivariateNormal(torch.zeros(self.no_basis - 1), (self.tau ** -1) * self.cov).sample()],
                 dim=0).view(self.no_out, self.no_basis)
-            self.W = gamma.view(self.no_basis, self.no_out)
+            self.W.data = gamma.view(self.no_basis, self.no_out)
 
         else:
             raise ValueError('Mode is incorreclty specified')
-
-        self.W_.data = self.W
-        self.tau_.data = self.tau
 
     def prior_log_prob(self):
         """
@@ -136,131 +130,76 @@ class GAM(Hidden):
         # p321 fahrmeir kneib lang:
         const = - 0.5 * (self.K.shape[0] - 1) * torch.log(self.tau_bij)
         kernel = -(2 * self.tau_bij) ** -1 * self.W.t() @ self.K @ self.W
-        return sum(const + kernel + self.dist['tau'].log_prob(self.tau)) # notice that tau can be on R if
+        return sum(const + kernel + self.dist['tau'].log_prob(self.tau))  # notice that tau can be on R if
         # self.bijected is true!
 
-    def plot1d(self, X, y, path=None, true_model=None, param=None, confidence=None, **kwargs):
-        import matplotlib.pyplot as plt
-        import pandas as pd
-        import seaborn as sns
-        # TODO Overwrite due to X & Z mismatch: plotting axes & forward
-
+    def plot(self, X, y, chain=None, path=None, **kwargs):
         Z = torch.tensor(get_design(X.numpy(), degree=2, no_basis=self.no_basis), dtype=torch.float32,
                          requires_grad=False)
+        df0 = self.predict_states(Z, chain)
 
-        # FIXME: in GAM case, Z is required for predictions & X is used for plotting
-        last_state = self.vec.detach().clone()  # saving the state before overwriting it
-        kwargs.update({'true': self._chain_predict([true_model], Z)['0']})
-        kwargs.update(self._chain_predict(param, Z))
-
-        df = pd.DataFrame({'X': torch.reshape(X, (X.shape[0],)).numpy()})
-        for name, var in kwargs.items():
-            df[name] = torch.reshape(var, (var.shape[0],)).numpy()
-        df = df.melt('X', value_name='y')
-        df = df.rename(columns={'variable': 'functions'})
-
-        # plot the functions
-        fig, ax = plt.subplots(nrows=1, ncols=1)
-        fig.subplots_adjust(hspace=0.5)
-        sns.scatterplot(
-            x=torch.reshape(X, (X.shape[0],)).numpy(),
-            y=torch.reshape(y, (y.shape[0],)).numpy(), ax=ax)
-
-        sns.lineplot('X', y='y', hue='functions', alpha=0.5, data=df, ax=ax)
-
-        if confidence is not None:  # plot (optional) confidence bands
-            ax.fill_between(**confidence, alpha=0.4, facecolor='lightblue')
-
-        fig.suptitle('Functions of the data')
-        self.vec_to_attrs(last_state)
+        df0['X'] = X.view(X.shape[0], ).numpy()
+        df1 = df0.melt('X', value_name='y')
+        df1 = df1.rename(columns={'variable': 'functions'})
+        plt = self._plot1d(X, y, df1, **kwargs)
 
         if path is None:
-            plt.plot()
+            plt.show()
         else:
             plt.savefig('{}.png'.format(path), bbox_inches='tight')
 
 
 if __name__ == '__main__':
+    from copy import deepcopy
     # dense example
     no_basis = 20
+    n = 1000
     X_dist = td.Uniform(-10., 10)
-    X = X_dist.sample(torch.Size([100]))
-    Z = torch.tensor(get_design(X.numpy(), degree=2, no_basis=no_basis), dtype=torch.float32, requires_grad=False)
+    X = X_dist.sample(torch.Size([n]))
+    Z = torch.tensor(get_design(X.numpy(), degree=2, no_basis=no_basis),
+                     dtype=torch.float32, requires_grad=False)
 
-    gam = GAM(no_basis=no_basis, order=1)
+    gam = GAM(no_basis=no_basis, order=1, activation=nn.Identity(), bijected=True)
+
+    gam.reset_parameters(tau=torch.tensor([0.01]))
+    gam.true_model = deepcopy(gam.state_dict())
+    y = gam.likelihood(Z).sample()
     gam.reset_parameters()
-    gam(Z)
+    gam.plot(X, y)
 
     gam.forward(Z)
-    y = gam.likelihood(Z).sample()
-
-    # prior log prob example
     gam.prior_log_prob()
 
-    gam.plot1d(X, y, true_model=gam.vec, param=
-    [torch.linspace(0., 10., len(gam.vec))+ td.Normal(torch.zeros_like(gam.vec), 1.).sample()])
+    step_size = 0.005
+    num_steps = 5000  # <-------------- important
+    pretrain = False
+    tune = False
+    burn_in = 2000
+    # num_chains 		type=int, 	default=1
+    num_chains = 1  # os.cpu_count() - 1
+    batch_size = 50
+    hmc_traj_length = 24
+    val_split = 0.9  # first part is train, second is val i.e. val_split=0.8 -> 80% train, 20% val
+    val_prediction_steps = 50
+    val_converge_criterion = 20
+    val_per_epoch = 200
 
-    # log_prob sg example
-    theta = flatten(gam)
-    # gam.closure_log_prob()
-    # gam.log_prob(Z, y, theta)
-    print('true:', theta)
+    # hmc = HMC(reg, X, y, X.shape[0],
+    #           step_size=step_size, num_steps=num_steps, burn_in=burn_in, pretrain=pretrain, tune=tune,
+    #           traj_length=hmc_traj_length,
+    #           num_chains=num_chains)
+    # hmc.sample()
+    from Pytorch.Samplers.LudwigWinkler import SGNHT
 
-    # log_prob full dataset example
-    gam.closure_log_prob(Z, y)
-    gam.log_prob(theta)
+    sgnht = SGNHT(gam, Z, y, X.shape[0],
+                  step_size, num_steps, burn_in, pretrain=pretrain, tune=tune,
+                  hmc_traj_length=hmc_traj_length,
+                  num_chains=num_chains)
+    sgnht.sample()
+    print(sgnht.chain)
 
-    gam.n_params
-    gam.p_names
-    unflatten(gam, flatten(gam))
+    # plot via stratified chain
+    import random
+    gam.plot(X[1:100], y[1:100], chain=random.sample(sgnht.chain, len(sgnht.chain)//10 ))
 
-    # plot 1D
-
-    # sample
-    import hamiltorch
-
-    # HMC NUTS
-    N = 2000
-    step_size = .3
-    L = 5
-    burn = 500
-    N_nuts = burn + N
-    params_hmc_nuts = hamiltorch.sample(
-        log_prob_func=gam.log_prob, params_init=theta,
-        num_samples=N_nuts, step_size=step_size, num_steps_per_sample=L,
-        sampler=hamiltorch.Sampler.HMC_NUTS, burn=burn,
-        desired_accept_rate=0.8)
-
-    chain_mat = torch.cat(params_hmc_nuts).reshape(len(params_hmc_nuts), -1)
-    print(chain_mat.mean(dim=0))
-
-    # evaluate log_probs
-    log_probs = torch.Tensor(len(params_hmc_nuts))
-    for i, tensor in enumerate(params_hmc_nuts):
-        log_probs[i] = gam.log_prob(tensor)
-
-    prior_log = torch.Tensor(len(params_hmc_nuts))
-    for i, tensor in enumerate(params_hmc_nuts):
-        gam.vec_to_attrs(tensor)
-        # print(gam.likelihood(Z).log_prob(y).sum())
-        prior_log[i] = gam.prior_log_prob()
-
-    # implicit RMHMC
-    N = 2000
-    hamiltorch.set_random_seed(123)
-
-    step_size = 0.15
-    num_samples = 50
-    num_steps_per_sample = 25
-    threshold = 1e-3
-    softabs_const = 10 ** 6
-    L = 25
-
-    params_irmhmc_bij = hamiltorch.sample(
-        log_prob_func=gam.log_prob, params_init=theta, num_samples=N,
-        step_size=step_size, num_steps_per_sample=L, sampler=hamiltorch.Sampler.RMHMC,
-        integrator=hamiltorch.Integrator.IMPLICIT, fixed_point_max_iterations=1000,
-        fixed_point_threshold=1e-05)
-
-    # Todo
-    #   use penalized K in GAM for prior_log_prob to improve identifiably
+    # Todo use penalized K in GAM for prior_log_prob to improve identifiably
