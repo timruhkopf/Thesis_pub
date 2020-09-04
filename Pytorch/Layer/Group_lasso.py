7a75d09eb1567bd54502ae03b655ef6d112f3d2b
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-09-04 15:07:44 +0200

    structuredModel  (is structured_BNN) works with default cdf(tau) alpha, not with alpha_prob.

--- Pytorch/Layer/Group_lasso.py
+++ Pytorch/Layer/Group_lasso.py
@@ -21,7 +21,7 @@ class Group_lasso(Hidden):
         """
         self.bijected = bijected
         Hidden.__init__(self, no_in, no_out, bias, activation)
-        self.dist['alpha'] = td.LogNormal(0., scale=1. / 20.)
+        self.dist['alpha'] = td.HalfCauchy(0.3)
 
     def define_model(self):
         self.m = self.no_out  # single "group size" to be penalized
@@ -47,7 +47,7 @@ class Group_lasso(Hidden):
         self.W = nn.Parameter(torch.Tensor(self.no_in - 1, self.no_out))
 
         # FIXME: check sigma dependence in W_shrinked: \beta_g | tau²_g, sigma² ~ MVN
-        self.dist['W_shrinked'] = td.Normal(torch.zeros(self.no_out), self.tau.clone().detach())
+        self.dist['W_shrinked'] = td.Normal(torch.zeros(self.no_out), self.tau ) #.clone().detach())
         self.dist['W'] = td.Normal(torch.zeros((self.no_in - 1) * self.no_out), torch.tensor([1.]))
 
         # add optional bias
@@ -63,26 +63,27 @@ class Group_lasso(Hidden):
 
     def update_distributions(self):
         """due to the hierarchical structure, the distributions parameters must be updated
-        Note, that this function is intended to be called immediately after vec_to_attr
-        in order to get thr correct log prob. This function does not """
-        # self.dist['tau'].__init__((self.m + 1) / 2, self.lamb ** 2)
-        # self.dist['W_shrinked'].__init__(torch.zeros(self.no_in), self.tau)
+        Note, that this function is intended to be called immediately after invoking
+        log_prob (see Model_util.log_prob) to take the updated parameters (via last
+        sampler.step and update the conditional distributions accordingly"""
 
         # BE WELL AWARE of the required inverse of the bijector in order to update
         # the conditional distributions accordingly
+        # print(self.tau)
+        # print(self.dist['tau'].transforms[0]._inverse(self.tau.clone().detach()))
 
         if self.bijected:
             self.dist['tau'].base_dist.rate = \
                 self.dist['lamb'].transforms[0]._inverse(self.lamb) ** 2 / 2
             self.dist['W_shrinked'].scale = \
-                self.dist['tau'].transforms[0]._inverse(self.tau.clone().detach())
+                self.dist['tau'].transforms[0]._inverse(self.tau.clone().detach()) # CAREFULL, THIS is ESSENTIAL for
+            # the model to run -and not invoke a second backward (in which already gradients have been lost)
         else:
             self.dist['tau'].rate = self.lamb ** 2 / 2
-            self.dist['W_shrinked'].scale = self.tau.clone().detach()
+            self.dist['W_shrinked'].scale = self.tau #.clone().detach()
 
     def reset_parameters(self, seperated=False):
         """sampling method to instantiate the parameters"""
-
         self.lamb.data = self.dist['lamb'].sample()
         self.update_distributions()  # to ensure tau's dist is updated properly
 
@@ -109,14 +110,8 @@ class Group_lasso(Hidden):
         """evaluate each parameter in respective distrib."""
 
         value = torch.tensor(0.)
-        # for name in self.p_names:
-        #     value += self.dist[name].log_prob(self.get_param(name)).sum()
-
-        value += self.dist['tau'].log_prob(self.tau).sum()
-        value += self.dist['b'].log_prob(self.b).sum()
-
-        value += self.dist['W_shrinked'].log_prob(self.W_shrinked).sum()
-        value += self.dist['W'].log_prob(self.W.reshape((self.no_in - 1) * self.no_out)).sum()
+        for name in self.p_names:
+            value += self.dist[name].log_prob(self.get_param(name)).sum()
 
         return value
 
@@ -127,11 +122,26 @@ class Group_lasso(Hidden):
         # FIXME alpha value for mu in likelihood depending on used shrinkage layer
 
         # as update_params already changed the tau value here explicitly
-        tau = self.dist['W_shrinked'].scale.clone().detach()
+        if self.bijected:
+            print(self.tau)
+            print(self.dist['W_shrinked'].scale)
+            # self.dist['W_shrinked'].scale += torch.tensor(1.)
+            tau = self.dist['tau'].transforms[0]._inverse(self.dist['W_shrinked'].scale)
+            print(tau)
+            print(self.dist['alpha'].cdf(tau))
+        else:
+            tau = self.dist['W_shrinked'].scale
+
+        tau.requires_grad_(False)
 
         # 1- : since small tau indicate high shrinkage & the possibility to
         # estimate using GAM, this means that alpha should be (close to) 1
-        return 1 - self.dist['alpha'].cdf(tau)
+        return  1 - self.dist['alpha'].cdf(tau)
+
+    # @property
+    # def alpha(self):
+    #     return torch.tensor(1.)
+
 
     @property
     def alpha_probab(self):
@@ -143,13 +153,21 @@ class Group_lasso(Hidden):
         favour alpha = 0 i.e. estimating x_1 in the bnn without gam (considering all
         interactions with other variables)
         """
-        tau = self.dist['W_shrinked'].scale
+        if self.bijected:
+            tau = self.dist['tau'].transforms[0]._inverse(self.dist['W_shrinked'].scale)
+        else:
+            tau = self.dist['W_shrinked'].scale
 
         # map tau to [0,1] interval, making it a probability
         # be careful as the mapping is informative prior knowledge!
         # Note further, that alpha is not learned!
-        pi = 1 - self.dist['alpha'].cdf(tau)
-        return td.Bernoulli(pi).sample()
+        pi = torch.tensor(1.) - self.dist['alpha'].cdf(tau)
+        if pi.item() < 0.01:
+            pi = torch.tensor([0.01])
+        print(pi)
+        delta = td.Bernoulli(pi).sample() # FIXME despite working on its own seems to cause an error in the sampler
+        print(delta)
+        return delta
 
 
 if __name__ == '__main__':
@@ -157,7 +175,7 @@ if __name__ == '__main__':
     no_in = 2
     no_out = 1
     n = 1000
-    X_dist = td.Uniform(torch.ones(no_in) * -10., torch.tensor(no_in) * 10.)
+    X_dist = td.Uniform(torch.ones(no_in) * -10., torch.ones(no_in) * 10.)
     X = X_dist.sample(torch.Size([n])).view(n, no_in)
 
     glasso = Group_lasso(no_in, no_out, bias=True, activation=nn.Identity(), bijected=True)
--- Pytorch/Models/StructuredModel_test.py
+++ Pytorch/Models/StructuredModel_test.py
@@ -1,77 +1,166 @@
 import torch
+import torch.nn as nn
 import torch.distributions as td
+import pandas as pd
 
-# slicing works gracefully
-# a = torch.ones([10, 10], requires_grad=True)
-# b = torch.ones([10, 3], requires_grad=True)
-# c = torch.ones([10, 7], requires_grad=True)
-#
-# d = (a[:, :3] ** 2 - b ** 2).sum()
-# e = (a[:, 3:] - c).sum()
-# L = d + e
-# L.backward()
-
-
+from Tensorflow.Effects.bspline import get_design
 from Pytorch.Models.GAM import GAM
 from Pytorch.Models.ShrinkageBNN import ShrinkageBNN
-from Pytorch.Models.BNN import BNN
-from Pytorch.Layer.Group_lasso import Group_lasso
-
+from Pytorch.Util.ModelUtil import Model_util
 
 import torch.nn as nn
 
 
-class H(nn.Module):
-    def __init__(self):
+class H(nn.Module, Model_util):
+    def __init__(self, hunits=[2, 3, 1], shrinkage='glasso',
+                 activation=nn.ReLU(), final_activation=nn.ReLU(),
+                 seperated=True, bijected=True,
+                 no_basis=20):
         super().__init__()
-        # both models are failing
-        # self.bnn = ShrinkageBNN(hunits=[2, 3, 1]) # group_lasso is the first layer here!
-        self.bnn = Group_lasso(2, 1, bijected=True)
+        self.no_basis = no_basis
+        self.hunits = hunits
+        self.activation = activation
+        self.final_activation = final_activation
+        self.seperated = seperated
+        self.bijected = bijected
 
-        # these models run with no problem at all
-        # self.bnn = BNN(hunits=[2, 3, 1])
-        self.gam = GAM(no_basis=20)
+        # define the model components
+        self.bnn = ShrinkageBNN(hunits, activation, final_activation, shrinkage, seperated, bijected)
+        self.gam = GAM(no_basis=no_basis)
 
-        self.bnn.reset_parameters()
-        self.gam.reset_parameters()
+        self.reset_parameters()
 
-    def forward(self, X):
-        return self.bnn.forward(X[:, :2]) + self.alpha * self.gam.forward(X[:, 2:])
+        self.a = list()
+
+    def forward(self, X, Z):
+        self.a.append(self.alpha)
+        return self.bnn.forward(X) + self.alpha * self.gam.forward(Z)
 
     @property
     def alpha(self):
-        if isinstance(self.bnn, BNN):
-            tau = self.bnn.layers[0].W[0, 0].clone().detach()
-        elif isinstance(self.bnn, Group_lasso):
-            tau = self.bnn.W[0, 0].clone().detach()  # notice, that this should be actually tau
+        return self.bnn.layers[0].alpha
 
-        tau.requires_grad_(False)
-        return tau
+    # @property
+    # def alpha(self):
+    #     return self.bnn.layers[0].alpha_probab
 
     def prior_log_prob(self):
         """surrogate for the hidden layers' prior log prob"""
         return self.bnn.prior_log_prob() + sum(self.gam.prior_log_prob())
 
-    def likelihood(self, X, sigma=torch.tensor(1.)):
+    def likelihood(self, X, Z, sigma=torch.tensor(1.)):
         """:param X: joint matrix, first cols are classic design, the following are
         the bspline extension desing marix"""
-        return td.Normal(self.forward(X), sigma)
+        return td.Normal(self.forward(X, Z), sigma)
 
-    def log_prob(self, X, y):
-        return self.prior_log_prob() + self.likelihood(X).log_prob(y).sum()
+    def log_prob(self, X, Z, y):
+        self.update_distributions()
+        return self.prior_log_prob() + self.likelihood(X, Z).log_prob(y).sum()
+
+    def reset_parameters(self, seperated=False, **kwargs):
+        # Resample the BNN part
+        self.bnn.reset_parameters(seperated)
+        self.gam.reset_parameters(**kwargs)
+
+    def update_distributions(self):
+        self.gam.update_distributions()
+        self.bnn.update_distributions()
+
+    @torch.no_grad()
+    def _predict_states(self, X, Z, chain=None):
+        """
+        predict f(X) based on true, current and chain's states.
+        a submethod of plotting
+
+        :param y: Torch.Tensor
+        :param chain: list of state_dicts
+        :param path: string: system path where to save to
+        :param *args: Torch.Tensor(s) upon which the predictions are made using
+        the model's forward method. Normal models require this to be the
+        design matrix X. In the case of structured BNN this must be X and Z
+
+        :return:
+        """
+
+        # (1) PREDICT functions -----------------------------------------------
+        df = pd.DataFrame()
+        df_gam = pd.DataFrame()
+
+        # predict current state
+        df['current'] = self.forward(X, Z).view(X.shape[0], ).numpy()
+        df_gam['current'] = self.gam.forward(Z).view(X.shape[0], ).numpy()
+        current = deepcopy(self.state_dict())
+
+        # predict true model
+        self.load_state_dict(self.true_model)
+        df['true'] = self.forward(X, Z).view(X.shape[0], ).numpy()
+        df_gam['true'] = self.gam.forward(Z).view(X.shape[0], ).numpy()
+
+        # predict chain
+        if chain is not None:
+            for i, c in enumerate(chain):
+                self.load_state_dict(c)
+                df[str(i)] = self.forward(X, Z).view(X.shape[0], ).numpy()
+                df_gam[str(i)] = self.gam.forward(Z).view(X.shape[0], ).numpy()
+
+            # return to current state
+            self.load_state_dict(current)
+
+        return df, df_gam
+
+    def plot(self, X, Z, y, chain=None, path=None, **kwargs):
+        # predict the entire model
+        df0, df1 = self._predict_states(X, Z, chain)  # df1 is gam only prediction
+
+
+        plt1 = self.plot2d(X, y, df0, **kwargs)
+
+        # predict merely the GAM part to ensure it does not deteriorate
+        df1['X'] = X[:, 0].view(X.shape[0], ).numpy()  # explicitly the first var is modeled by gam
+        df1 = df1.melt('X', value_name='y')
+        df1 = df1.rename(columns={'variable': 'functions'})
+        plt2 = self._plot1d(X, y=None, df=df1, **kwargs)
+
+        if path is None:
+            plt1.show()
+            plt2.show()
+        else:
+            plt1.savefig('{}.png'.format(path), bbox_inches='tight')
+            plt2.savefig('{}_GAM.png'.format(path), bbox_inches='tight')
+
+
 
 
 if __name__ == '__main__':
-    X = torch.ones([10, 22])
+    from copy import deepcopy
 
-    h = H()
+    # parametrize the basic datamodel
+    no_basis = 20
+    no_in = 2
+    no_out = 1
+    n = 1000
+
+    # generate data
+    X_dist = td.Uniform(torch.ones(no_in) * (-10.), torch.ones(no_in) * 10.)
+    X = X_dist.sample(torch.Size([n])).view(n, no_in)
+    Z = torch.tensor(get_design(X[:, 0].numpy(), degree=2, no_basis=no_basis),
+                     dtype=torch.float32)
+
+    # X_joint = torch.cat([X, Z], dim=1)
+    # X_joint.requires_grad_(True)
+
+    h = H(hunits=[2, 10, 1], bijected=True)
+    h.reset_parameters(True)
+    h.true_model = deepcopy(h.state_dict())
     # L = h(X).sum()
     # L.backward()
+
 
     # h.alpha
     # h.h1.layers
 
-    y = h.likelihood(X).sample([len(X)])
+    y = h.likelihood(X,Z).sample()
+    # h.plot(X, Z, y) # Fixme: canvas fails
 
     # check sampling ability.
     from Pytorch.Samplers.mygeoopt import myRHMC, mySGRHMC, myRSGLD
@@ -79,8 +168,8 @@ if __name__ == '__main__':
 
     burn_in, n_samples = 100, 1000
 
-    trainset = TensorDataset(X, y)
-    trainloader = DataLoader(trainset, batch_size=10, shuffle=True, num_workers=0)
+    trainset = TensorDataset(X, Z, y)
+    trainloader = DataLoader(trainset, batch_size=n, shuffle=True, num_workers=0)
 
     Sampler = {'RHMC': myRHMC,  # epsilon, n_steps
                'SGRLD': myRSGLD,  # epsilon
@@ -91,7 +180,15 @@ if __name__ == '__main__':
     # glasso.plot(X_joint, y)
 
     torch.autograd.set_detect_anomaly(True)
+    h.reset_parameters()
     sampler = Sampler(h, epsilon=0.001, L=2)
     sampler.sample(trainloader, burn_in, n_samples)
 
-    [(name, p.grad) for name, p in self.model.named_parameters()]
+    print(sampler.chain_mat)
+    print(sampler.model.a)
+    import random
+    h.plot(X[:100], Z[:100], y[:100],  chain=random.sample(sampler.chain, 100),
+                **{'title': 'structuredBNN'})
+
+
+    # [(name, p.grad) for name, p in self.model.named_parameters()]
