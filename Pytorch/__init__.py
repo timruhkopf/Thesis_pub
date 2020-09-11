0101a4f0c891883b1725dd11ff1eeab96f43cad1
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-09-11 13:03:13 +0200

    depreciation of shell scripts and the Structured_BNN.py working with split design

---
+++ Pytorch/__init__.py
@@ -0,0 +1,9 @@
+# dependencies and naming conventions
+import torch
+import torch.nn as nn
+import torch.distributions as td
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import deepcopy
