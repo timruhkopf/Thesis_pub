4b36af8c877272470041dd66144cda004c18e286
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-09-26 16:34:35 +0200

    introduced a new more compact way to define the distributional assumptions, by writing them immediately on the parameter

--- Pytorch/Layer/Hidden.py
+++ Pytorch/Layer/Hidden.py
@@ -37,35 +37,30 @@ class Hidden(nn.Module, Model_util):
         self.reset_parameters()
         self.true_model = None
 
-    # (1) USER MUST DEFINE THESE FUNCTIONS: ------------------------------------
-    # TO MAKE THIS A VEC MODEL
     def define_model(self):
-        self.tau_w = 1.
-        self.dist = {'W': td.MultivariateNormal(
-            torch.zeros(self.no_in * self.no_out),
-            self.tau_w * torch.eye(self.no_in * self.no_out))}  # todo refactor this to td.Normal()
+        self.tau_w = torch.tensor([1.])
+
+        # self.tau_w = nn.Parameter(torch.tensor([1.]))
+        # self.tau_w.distrib = td.Gamma(torch.tensor([2.]), torch.tensor([1.]))
+        # self.tau_w.data = self.tau_w.distrib.sample()
 
         self.W = nn.Parameter(torch.Tensor(self.no_in, self.no_out))
+        self.W.distrib = td.Normal(loc=torch.zeros_like(self.W.data), scale=self.tau_w)
 
         if self.has_bias:
-            self.tau_b = 1.
-            self.dist['b'] = td.Normal(0., self.tau_b)
             self.b = nn.Parameter(torch.Tensor(self.no_out))
+            self.b.distrib = td.Normal(torch.zeros(self.no_out), 1.)
 
     @torch.no_grad()
     def reset_parameters(self):
-        # nn.init.xavier_normal_(self.W)
-        # with torch.no_grad():
-        nn.init._no_grad_zero_(self.W)
-        self.W.add_(self.dist['W'].sample().view(self.W.shape))
-
-        if self.has_bias:
-            nn.init.normal_(self.b)
+        for p in self.parameters():
+            p.data = p.distrib.sample()
 
         self.init_model = deepcopy(self.state_dict())
 
     def update_distributions(self):
         # here no hierarchical distributions exists
+        # self.W.distrib.scale = torch.ones_like(self.W.distrib.scale) * self.tau_w
         return None
 
     # (2) DEFINE THESE FUNCTIONS IN REFERENCE TO SURROGATE PARAM: --------------
@@ -78,37 +73,10 @@ class Hidden(nn.Module, Model_util):
 
     def prior_log_prob(self):
         """evaluate each parameter in respective distrib."""
-        param_names = self.p_names
-        param_names.remove('W')
-
         value = torch.tensor(0.)
-        if param_names is not None:
-            for name in param_names:
-                value += self.dist[name].log_prob(self.get_param(name)).sum()
-
-        value += self.dist['W'].log_prob(self.W.view(self.W.nelement()))
-
+        for p in self.parameters():
+            value += p.distrib.log_prob(p).sum()
         return value
-
-    def likelihood(self, X):
-        """:returns the conditional distribution of y | X"""
-        # TODO update likelihood to become an attribute distribution,
-        #  which is updated via self.likelihood.__init__(newloc, scale)
-        #  or even use self.likelihood.loc = newloc
-        return td.Normal(self.forward(X), scale=torch.tensor(1.))
-
-    def my_log_prob(self, X, y):
-        """
-        SG flavour of Log-prob: any batches of X & y can be used
-        make sure to pass self.log_prob to the sampler, since self.my_log_prob
-        is a convenience mask
-
-         self.log_prob(X,y), which returns the log_prob with current state of
-        'parameters'. This is particularly handy with optim based samplers,
-        since 'parameters' are literally nn.Parameters and update their state based
-        on optim proposals (always up to date)"""
-        return self.prior_log_prob().sum() + \
-               self.likelihood(X).log_prob(y).sum()
 
 
 class Hidden_flat(Hidden):
@@ -117,28 +85,22 @@ class Hidden_flat(Hidden):
         formulate the model with truncated flat priors
         :return:
         """
-        self.dist = {'W': td.Uniform(torch.ones(self.no_in, self.no_out) * -100.,
-                                     torch.ones(self.no_in, self.no_out) * 100)}  # todo refactor this to td.Normal()
-
         self.W = nn.Parameter(torch.Tensor(self.no_in, self.no_out))
+        self.W.distrib = td.Uniform(torch.ones(self.no_in, self.no_out) * -100.,
+                                    torch.ones(self.no_in, self.no_out) * 100)
 
         if self.has_bias:
-            self.dist['b'] = td.Uniform(torch.ones(self.no_out) * -100.,
-                                        torch.ones(self.no_out) * 100)
             self.b = nn.Parameter(torch.Tensor(self.no_out))
+            self.b.distrib = td.Uniform(torch.ones(self.no_out) * -100.,
+                                        torch.ones(self.no_out) * 100)
 
-    def prior_log_prob(self):
-        value = torch.tensor(0.)
-        for name in self.p_names:
-            value += self.dist[name].log_prob(self.get_param(name)).sum()
 
-        return value
 
 
 if __name__ == '__main__':
     from copy import deepcopy
 
-    no_in = 1
+    no_in = 2
     no_out = 1
     n = 1000
 
@@ -194,9 +156,9 @@ if __name__ == '__main__':
             for eps in np.arange(0.001, 0.03, 0.002):
                 model.reset_parameters()
                 name = '{}_{}_{}_{}'.format(sampler_name, str(eps), str(L), str(rep))
-                path = '/home/tim/PycharmProjects/Thesis/Pytorch/Experiments/Results_Hidden1/'
+                path = '/home/tim/PycharmProjects/Thesis/Pytorch/Experiments/Results/Results_Hidden1/'
                 sampler_param = dict(
-                    epsilon=eps, num_steps=1000, burn_in=400,
+                    epsilon=eps, num_steps=100, burn_in=40,
                     pretrain=False, tune=False, num_chains=1)
 
                 if sampler_name in ['SGNHT', 'RHMC', 'SGRHMC']:
@@ -278,23 +240,23 @@ if __name__ == '__main__':
     # # lag = np.arange(len(sgnht.acf))
     # plt.plot(lag, a)
     # plt.show()
-                #
-                # tsaplots.plot_acf(x, lags=100)
-                #
-                # # my own trials on autocorrelation plots
-                # x = sgnht.chain_mat
-                # x = x - np.mean(x, axis=0)
-                # x = x[:, 0]
-                #
-                # # Display the autocorrelation plot of your time series
-                # fig = tsaplots.plot_acf(x, lags=1000, fft=True)
-                # plt.show()
-                #
-                # import tidynamics
-                #
-                # sgnht.acf = tidynamics.acf(x)[1: len(sgnht.chain) // 3]
-                # lag = np.arange(len(sgnht.acf))
-                # plt.plot(lag, sgnht.acf)
-                # plt.show()
-                #
-                # sgnht.fast_autocorr(0)
+    #
+    # tsaplots.plot_acf(x, lags=100)
+    #
+    # # my own trials on autocorrelation plots
+    # x = sgnht.chain_mat
+    # x = x - np.mean(x, axis=0)
+    # x = x[:, 0]
+    #
+    # # Display the autocorrelation plot of your time series
+    # fig = tsaplots.plot_acf(x, lags=1000, fft=True)
+    # plt.show()
+    #
+    # import tidynamics
+    #
+    # sgnht.acf = tidynamics.acf(x)[1: len(sgnht.chain) // 3]
+    # lag = np.arange(len(sgnht.acf))
+    # plt.plot(lag, sgnht.acf)
+    # plt.show()
+    #
+    # sgnht.fast_autocorr(0)
