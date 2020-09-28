9cad3240ff4214af3d42eaf3f74c5b1ef5ff47dc
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-09-28 10:20:54 +0200

    Depreciated TENSORFLOW

--- Tensorflow/Effects/__init__.py
+++
@@ -1,4 +0,0 @@
-# When is Tensorflow.Effects imported? in case of creating new Cases
-from Tensorflow.Effects.Effect1D import Effects1D
-from Tensorflow.Effects.Effect2D import Effects2D
-from Tensorflow.Effects.bspline import *
