a54637afe3c8bfbd85f603e2b15c88f141ef2b8a
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-10-02 16:31:47 +0200

    constant alpha

--- Pytorch/Grid/Suits/Suit_Structured.py
+++ Pytorch/Grid/Suits/Suit_Structured.py
@@ -15,12 +15,12 @@ n_val = 100
 batch = 100
 
 # ALPHA CDF ------------------------------------------
-model_param = dict(hunits=[2, 10, 5, 1], activation=nn.ReLU(),
-                   final_activation=nn.Identity(), shrinkage='ghorse',
-                   no_basis=20, seperated=True, bijected=True, alpha_type='cdf')
-
-samplers(cls, cls_Grid, n, n_val, model_param, steps, batch, epsilons=np.arange(0.0001, 0.02, 0.002),
-         Ls=[1, 2, 3], repeated=15)
+# model_param = dict(hunits=[2, 10, 5, 1], activation=nn.ReLU(),
+#                    final_activation=nn.Identity(), shrinkage='ghorse',
+#                    no_basis=20, seperated=True, bijected=True, alpha_type='cdf')
+#
+# samplers(cls, cls_Grid, n, n_val, model_param, steps, batch, epsilons=np.arange(0.0001, 0.02, 0.002),
+#          Ls=[1, 2, 3], repeated=15)
 
 # ALPHA CONSTANT -------------------------------------
 model_param = dict(hunits=[2, 10, 5, 1], activation=nn.ReLU(),
