a3147bacf182aec002ad9e2949e7d9c481277dbb
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-10-02 17:54:02 +0200

    Suit continue for BNN & Ghorse & Hidden of git = '390663d9'

--- Pytorch/Grid/Suits/Suit_GHorse.py
+++ Pytorch/Grid/Suits/Suit_GHorse.py
@@ -14,26 +14,26 @@ batch = 100
 cls = Group_HorseShoe
 cls_Grid = GRID_Layout
 
-model_param = dict(no_in=2, no_out=1, bias=True, activation=nn.ReLU(), bijected=True)
-
-samplers(cls, cls_Grid, n, n_val, model_param, steps, batch,
-         epsilons=np.arange(0.0001, 0.02, 0.002),
-         Ls=[1, 2, 3], repeated=30)
+# model_param = dict(no_in=2, no_out=1, bias=True, activation=nn.ReLU(), bijected=True)
+#
+# samplers(cls, cls_Grid, n, n_val, model_param, steps, batch,
+#          epsilons=np.arange(0.0001, 0.02, 0.002),
+#          Ls=[1, 2, 3], repeated=30)
 
 # (Continuation) ----------------------
 import os
-#
-# git = '0365244'  # hash for folder to continue
+
+git = '390663d'  # hash for folder to continue
 # base = '/'.join(os.path.abspath(__file__).split('/')[:-3])
 # rooting = base + '/Experiment/Result_{}'.format(git)
-# # rooting = '/usr/users/truhkop/Thesis/Pytorch/Experiment/Result_0365244'
-#
-# grid = cls_Grid(root=rooting)
-# m = grid.find_successfull(path=rooting,
-#                           model=cls.__name__)
+rooting = '/usr/users/truhkop/Thesis/Pytorch/Experiment/Result_{}'.format(git)
+
+grid = cls_Grid(root=rooting)
+m = grid.find_successfull(path=rooting,
+                          model=cls.__name__)
 # m = {'Grouped_HorseShoe_MALA': m['Grouped_HorseShoe_MALA'],
 #      'Grouped_HorseShoe_SGLD': m['Grouped_HorseShoe_SGLD'],
 #      'Grouped_HorseShoe_SGNHT': m['Grouped_HorseShoe_SGNHT']}
-# print(m)
-# grid.continue_sampling_successfull(
-#     n=1000, n_val=100, n_samples=1000, burn_in=1000, models=m)
+print(m)
+grid.continue_sampling_successfull(
+    n=1000, n_val=100, n_samples=1000, burn_in=1000, models=m)
