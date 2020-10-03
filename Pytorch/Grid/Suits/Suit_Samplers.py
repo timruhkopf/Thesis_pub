fa1e6203b68678d3a6e583516f8db5e97fcfe17a
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-10-03 12:56:31 +0200

    start Ghorse & structured constant with all but sgld sgnht, which already succeeded

--- Pytorch/Grid/Suits/Suit_Samplers.py
+++ Pytorch/Grid/Suits/Suit_Samplers.py
@@ -25,79 +25,44 @@ def samplers(cls, cls_Grid, n, n_val, model_param, steps, batch, epsilons, Ls, s
             print('file result existed, still continuing')
 
     # # (3) RHMC -------------------------------------------------
-    #
-    # run = name + '_RHMC'
-    # root = rooting(run)
-    # bnn_unittest = cls_Grid(root)
-    # prelim_configs = bnn_unittest.grid_exec_RHMC(steps=steps, epsilons=epsilons, Ls=Ls)
-    #
-    # for config in prelim_configs:
-    #     for i in range(repeated):
-    #         config_copy = {key: deepcopy(value) for key, value in config.items()}
-    #         bnn_unittest.main(
-    #             seperated=seperated,
-    #             n=n, n_val=n_val,
-    #             model_class=cls, model_param=model_param,
-    #             sampler_name='RHMC', sampler_param=config_copy)
-    #
-    # # (4) SGRLD -------------------------------------------------
-    # run = name + '_SGRLD'
-    # root = rooting(run)
-    #
-    # bnn_unittest = cls_Grid(root)
-    # prelim_configs = bnn_unittest.grid_exec_SGRLD(steps=steps, batch_size=batch, epsilons=epsilons)
-    #
-    # for config in prelim_configs:
-    #     for i in range(repeated):
-    #         config_copy = {key: deepcopy(value) for key, value in config.items()}
-    #         bnn_unittest.main(
-    #             seperated=seperated,
-    #             n=n, n_val=n_val,
-    #             model_class=cls, model_param=model_param,
-    #             sampler_name='SGRLD', sampler_param=config_copy)
+
+    run = name + '_RHMC'
+    root = rooting(run)
+    bnn_unittest = cls_Grid(root)
+    prelim_configs = bnn_unittest.grid_exec_RHMC(steps=steps, epsilons=epsilons, Ls=Ls)
+
+    for config in prelim_configs:
+        for i in range(repeated):
+            config_copy = {key: deepcopy(value) for key, value in config.items()}
+            bnn_unittest.main(
+                seperated=seperated,
+                n=n, n_val=n_val,
+                model_class=cls, model_param=model_param,
+                sampler_name='RHMC', sampler_param=config_copy)
+
+    # (4) SGRLD -------------------------------------------------
+    run = name + '_SGRLD'
+    root = rooting(run)
+
+    bnn_unittest = cls_Grid(root)
+    prelim_configs = bnn_unittest.grid_exec_SGRLD(steps=steps, batch_size=batch, epsilons=epsilons)
+
+    for config in prelim_configs:
+        for i in range(repeated):
+            config_copy = {key: deepcopy(value) for key, value in config.items()}
+            bnn_unittest.main(
+                seperated=seperated,
+                n=n, n_val=n_val,
+                model_class=cls, model_param=model_param,
+                sampler_name='SGRLD', sampler_param=config_copy)
 
     # (0) SGNHT --------------------------------------------------------------------
-
-    run = name + '_SGNHT'
-    root = rooting(run)
-
-    bnn_unittest = cls_Grid(root)
-    prelim_configs = bnn_unittest.grid_exec_SGNHT(epsilons=epsilons, Ls=Ls, steps=steps, batch_size=batch)
-
-    for config in prelim_configs:
-        for i in range(repeated):
-            config_copy = {key: deepcopy(value) for key, value in config.items()}
-            bnn_unittest.main(
-                seperated=seperated,
-                n=n, n_val=n_val,
-                model_class=cls, model_param=model_param,
-                sampler_name='SGNHT', sampler_param=config_copy)
-
-
-    # (2) SGLD -------------------------------------------------
-
-    run = name + '_SGLD'
-    root = rooting(run)
-
-    bnn_unittest = cls_Grid(root)
-    prelim_configs = bnn_unittest.grid_exec_SGLD(steps=steps, epsilons=epsilons)
-
-    for config in prelim_configs:
-        for i in range(repeated):
-            config_copy = {key: deepcopy(value) for key, value in config.items()}
-            bnn_unittest.main(
-                seperated=seperated,
-                n=n, n_val=n_val,
-                model_class=cls, model_param=model_param,
-                sampler_name='SGLD', sampler_param=config_copy)
-
-    # # (5) SGRHMC -------------------------------------------------
     #
-    # run = name + '_SGRHMC'
+    # run = name + '_SGNHT'
     # root = rooting(run)
     #
     # bnn_unittest = cls_Grid(root)
-    # prelim_configs = bnn_unittest.grid_exec_SGRHMC(steps=steps, batch_size=batch, epsilons=epsilons, Ls=Ls)
+    # prelim_configs = bnn_unittest.grid_exec_SGNHT(epsilons=epsilons, Ls=Ls, steps=steps, batch_size=batch)
     #
     # for config in prelim_configs:
     #     for i in range(repeated):
@@ -106,15 +71,16 @@ def samplers(cls, cls_Grid, n, n_val, model_param, steps, batch, epsilons, Ls, s
     #             seperated=seperated,
     #             n=n, n_val=n_val,
     #             model_class=cls, model_param=model_param,
-    #             sampler_name='SGRHMC', sampler_param=config_copy)
+    #             sampler_name='SGNHT', sampler_param=config_copy)
     #
-    # # (1) MALA -------------------------------------------------
     #
-    # run = name + '_MALA'
+    # # (2) SGLD -------------------------------------------------
+    #
+    # run = name + '_SGLD'
     # root = rooting(run)
     #
     # bnn_unittest = cls_Grid(root)
-    # prelim_configs = bnn_unittest.grid_exec_MALA(steps=steps, epsilons=epsilons)
+    # prelim_configs = bnn_unittest.grid_exec_SGLD(steps=steps, epsilons=epsilons)
     #
     # for config in prelim_configs:
     #     for i in range(repeated):
@@ -123,4 +89,38 @@ def samplers(cls, cls_Grid, n, n_val, model_param, steps, batch, epsilons, Ls, s
     #             seperated=seperated,
     #             n=n, n_val=n_val,
     #             model_class=cls, model_param=model_param,
-    #             sampler_name='MALA', sampler_param=config_copy)
+    #             sampler_name='SGLD', sampler_param=config_copy)
+
+    # (5) SGRHMC -------------------------------------------------
+
+    run = name + '_SGRHMC'
+    root = rooting(run)
+
+    bnn_unittest = cls_Grid(root)
+    prelim_configs = bnn_unittest.grid_exec_SGRHMC(steps=steps, batch_size=batch, epsilons=epsilons, Ls=Ls)
+
+    for config in prelim_configs:
+        for i in range(repeated):
+            config_copy = {key: deepcopy(value) for key, value in config.items()}
+            bnn_unittest.main(
+                seperated=seperated,
+                n=n, n_val=n_val,
+                model_class=cls, model_param=model_param,
+                sampler_name='SGRHMC', sampler_param=config_copy)
+
+    # (1) MALA -------------------------------------------------
+
+    run = name + '_MALA'
+    root = rooting(run)
+
+    bnn_unittest = cls_Grid(root)
+    prelim_configs = bnn_unittest.grid_exec_MALA(steps=steps, epsilons=epsilons)
+
+    for config in prelim_configs:
+        for i in range(repeated):
+            config_copy = {key: deepcopy(value) for key, value in config.items()}
+            bnn_unittest.main(
+                seperated=seperated,
+                n=n, n_val=n_val,
+                model_class=cls, model_param=model_param,
+                sampler_name='MALA', sampler_param=config_copy)
