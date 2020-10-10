71d557121df56eddf095eb98a92605ffe85c4316
Tim Ruhkopf <tim.ruhkopf@googlemail.com>
2020-10-10 16:56:24 +0200

    INIT & Continue of GAM bijected = True

--- Pytorch/Grid/Suits/Suit_GAM.py
+++ Pytorch/Grid/Suits/Suit_GAM.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from Pytorch.Layer.GAM import GAM
 from Pytorch.Grid.Grid_GAM_Cases import GRID_Layout_GAM
 from Pytorch.Grid.Suits.Suit_Samplers import samplers
+from subprocess import check_output
 
 # (CONFIG) ---------------------------------------------------------------------
 steps = 1000
@@ -16,23 +17,24 @@ cls = GAM
 cls_Grid = GRID_Layout_GAM
 
 model_param = dict(xgrid=(0, 10, 0.5), order=1, no_basis=20, no_out=1,
-                   activation=nn.Identity(), bijected=False)
+                   activation=nn.Identity(), bijected=True)
 
 samplers(cls, cls_Grid, n, n_val, model_param, steps, batch,
          epsilons=np.arange(0.0001, 0.02, 0.002),
-         Ls=[1, 2, 3], repeated=15)
+         Ls=[1, 2, 3], repeated=10)
 
 # (Continuation) ----------------------
 import os
 
 # git = '56200a5'  # hash for folder to continue
+git = '{}'.format(check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip())
 # base = '/'.join(os.path.abspath(__file__).split('/')[:-3])
 # rooting = base + '/Experiment/Result_{}'.format(git)
-# rooting = '/usr/users/truhkop/Thesis/Pytorch/Experiment/Result_0365244'
-#
-# grid = cls_Grid(root=rooting)
-# m = grid.find_successfull(path=rooting,
-#                           model=cls.__name__)
-#
-# grid.continue_sampling_successfull(
-#     n=1000, n_val=100, n_samples=10000, burn_in=10000, models=m)
+rooting = '/usr/users/truhkop/Thesis/Pytorch/Experiment/Result_{}'.format(git)
+
+grid = cls_Grid(root=rooting)
+m = grid.find_successfull(path=rooting,
+                          model=cls.__name__)
+
+grid.continue_sampling_successfull(
+    n=1000, n_val=100, n_samples=10000, burn_in=10000, models=m)
--- Pytorch/Util/Util_Model.py
+++ Pytorch/Util/Util_Model.py
@@ -92,7 +92,7 @@ class Util_Model(Util_plots):
             print(chain[0], chain[-1])
             raise RuntimeError('first and last entry contain nan')
 
-        if len(chain) > 2 and all(all(a == b) for a, b in zip(chain[0].values(), chain[-2].values())):
+        if len(chain) > 2 and all(torch.all(a == b) for a, b in zip(chain[0].values(), chain[-2].values())):
             print(chain[0], chain[-2])
             # FIXME: as it appears, many chains actually progress usefully,
             #  but 1st and last are same - this seems to be a technical issue yet to be identified!
